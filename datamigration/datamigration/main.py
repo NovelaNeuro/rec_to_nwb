6581a6633730c99bad65cb56b05cb0f4452cd905
alexcwikla <aleks.cwikla@gmail.com>
2019-10-02 14:02:31 +0200

    Grab rec file
    + download from passed arg
    + if file not exist, download from default url

--- datamigration/datamigration/main.py
+++ datamigration/datamigration/main.py
@@ -1,1 +1,26 @@
-# main.py
+import pathlib
+import sys
+import requests
+
+
+def download_file(url):
+    headers = {'user-agent': 'Wget/1.16 (linux-gnu)'}
+    r = requests.get(url, stream=True, headers=headers)
+    with open("../data/file.rec", 'wb') as f:
+        for chunk in r.iter_content(chunk_size=1024):
+            if chunk:
+                f.write(chunk)
+
+
+def main():
+
+    if len(sys.argv) > 1:
+        print('Downloading file from passed url')
+        download_file(str(sys.argv[1]))
+    elif not pathlib.Path('../data/file.rec').exists():
+        print('Downloading default file.rec')
+        download_file("https://www.dropbox.com/s/a7ddruxoffgbe7y/AC13_d27_WhereAreWeNow.rec?dl=1")
+
+
+if __name__ == "__main__":
+   main()
