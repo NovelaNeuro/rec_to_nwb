3bbf34cdd392c9b8ff190be36e64fd2d852afd3d
acwikla <acwikla@novelaneuro.com>
2019-10-03 15:01:02 +0200

    Added Class

--- datamigration/datamigration/main.py
+++ datamigration/datamigration/main.py
@@ -3,45 +3,46 @@ import pathlib
 import xml.etree.ElementTree as et
 
 
-def open_xml():
-    file_path = '../data/fl_lab_header.xml'
-    if pathlib.Path(file_path).exists():
-        with open(file_path, 'r') as f:
-            tree = et.parse(f)
-            return tree
+class XMLParser:
+    file_path = ''
 
+    def __init__(self, file_path='../data/fl_lab_header.xml'):
+        self.file_path = file_path
 
-def get_root():
-    configuration = open_xml().getroot()
-    return configuration
+    def open_xml(self):
+        if pathlib.Path(self.file_path).exists():
+            with open(self.file_path, 'r') as f:
+                tree = et.parse(f)
+                return tree
 
+    def get_root(self):
+        configuration = self.open_xml().getroot()
+        return configuration
 
-def get_element(name):
-    path = "./" + name
-    element = get_root().findall(path)
-    return element
+    def get_element(self, name):
+        path = "./" + name
+        element = self.get_root().findall(path)
+        return element
 
+    def get_elements(self, name):
+        path = "./" + name + '/'
+        elements = []
+        for i in self.get_root().findall(path):
+            elements.append(i)
+        return elements
 
-def get_elements(name):
-    path = "./" + name + '/'
-    elements = []
-    for i in get_root().findall(path):
-        elements.append(i)
-    return elements
+    def get_element_specification(self, name):
+        elements = []
+        for element in self.open_xml().iter(tag=name):
+            elements.append(element)
+        return elements
 
-
-def get_element_specification(name):
-    elements = []
-    for element in open_xml().iter(tag=name):
-        elements.append(element)
-    return elements
-
-
-def show_all():
-    children = get_root().getchildren()
-    for child in children:
-        et.dump(child)
+    def show_all(self, ):
+        children = self.get_root().getchildren()
+        for child in children:
+            et.dump(child)
 
 
 if __name__ == '__main__':
-    show_all()
+    xml_parser = XMLParser()
+    xml_parser.show_all()
