4c49423f48aeb481d040749a5a30e7da6f74ba95
acwikla-novela <55695978+acwikla-novela@users.noreply.github.com>
2019-10-22 15:37:29 +0200

    Travis CI

--- src/test/datamigration/test_header_view.py
+++ src/test/datamigration/test_header_view.py
@@ -1,3 +1,5 @@
+import sys
+sys.path.append('../src')
 import unittest
 
 from src.datamigration.header.module import header
@@ -6,7 +8,7 @@ from src.datamigration.header.module import header
 class TestHeaderInterface(unittest.TestCase):
 
     def setUp(self):
-        self.header = header.Header('fl_lab_sample_header.xml')
+        self.header = header.Header('src/test/datamigration/fl_lab_sample_header.xml')
 
     def test_configuration_tag(self):
         configuration = self.header.configuration
