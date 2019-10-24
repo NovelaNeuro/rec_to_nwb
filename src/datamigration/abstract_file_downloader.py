410fe80a4f6f6f09d2c0b22da85a776c5a7ed369
acwikla-novela <55695978+acwikla-novela@users.noreply.github.com>
2019-10-24 15:04:33 +0200

    Grab rec file to the project

---
+++ src/datamigration/abstract_file_downloader.py
@@ -0,0 +1,31 @@
+""" Module with Interface to download a file
+
+Class:
+    AbstractFileDownloader()
+"""
+from abc import abstractmethod
+
+
+class AbstractFileDownloader:
+    """ Interface to download a file
+
+        Abstract methods:
+            download_file()
+            start_downloading()
+    """
+
+    @abstractmethod
+    def download_file(self, alternative_url):
+        """ Check parameters and call start_downloading(url)
+
+            Parameters:
+                :param alternative_url: custom url to file
+        """
+
+    @abstractmethod
+    def start_downloading(self, url):
+        """ Start downloading from url
+
+            Parameters:
+                :param url: url to file
+        """
