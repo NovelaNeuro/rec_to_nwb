d015534811c754caed98585becb0e901472c74b2
acwikla-novela <55695978+acwikla-novela@users.noreply.github.com>
2019-10-29 15:10:07 +0100

    Processing integration test

---
+++ src/test/datamigration/test_processing_integration.py
@@ -0,0 +1,11 @@
+import os
+import unittest
+
+
+class ProcessingIntegrationTest(unittest.TestCase):
+
+    @unittest.skipIf(os.environ.get("TRAVIS") is not None,
+                     reason="Skipping ProcessingIntegrationTest on Travis CI")
+    def test_processing_integration(self):
+        assert 1 == 1
+
