cf0b91f7fc3f877e918514b1aaa8d8f0cac2a989
acwikla-novela <55695978+acwikla-novela@users.noreply.github.com>
2019-10-29 15:10:07 +0100

    Merge pull request #14 from NovelaNeuro/user/acwikla/Grab-Rec-file-to-the-project
    User/acwikla/grab rec file to the project

--- src/test/datamigration/test_processing_integration.py
+++ src/test/datamigration/test_processing_integration.py
@@ -4,7 +4,7 @@ import unittest
 
 class ProcessingIntegrationTest(unittest.TestCase):
 
-    @unittest.skipIf(os.environ.get("skipProcessingTest") is not True,
+    @unittest.skipIf(os.environ.get("TRAVIS") is not None,
                      reason="Skipping ProcessingIntegrationTest on Travis CI")
     def test_processing_integration(self):
         assert 1 == 1
