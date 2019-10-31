371ff3789f862ab8898f9b88dd7906da00bc4ca6
acwikla-novela <55695978+acwikla-novela@users.noreply.github.com>
2019-10-31 14:12:56 +0100

    add pos int test

--- src/e2etests/integration/experiment_data.py
+++ src/e2etests/integration/experiment_data.py
@@ -1,4 +1,6 @@
 class ExperimentData:
-    animal_name = 'donald'
+    animal_name = 'beans'
     mda_folder = 'preprocessing/20190718/20190718_beans_01_s1.mda'
-    root_path = '../test_data/' + animal_name + '/' + mda_folder + '/'
+    pos_folder = '20190718_beans_01_s1.1.pos/'
+    pos_file = '20190718_beans_01_s1.1.pos_online.dat'
+    root_path = '../test_data/' + animal_name + '/' + 'preprocessing/20190718/'
---
+++ src/e2etests/integration/test_pos.py
@@ -0,0 +1,26 @@
+import unittest
+
+import pandas as pd
+from experiment_data import ExperimentData
+from pynwb.behavior import Position
+from rec_to_binaries.read_binaries import readTrodesExtractedDataFile
+
+
+class TestPOSMigration(unittest.TestCase):
+
+    def setUp(self):
+        print('Test requires preprocessed test_data folder at e2etests location')
+        self.path_to_beans = ExperimentData.root_path + ExperimentData.pos_folder + ExperimentData.pos_file
+        self.pos_online = readTrodesExtractedDataFile(self.path_to_beans)
+
+    def test_reading_pos(self):
+        position_online = pd.DataFrame(self.pos_online['data'])
+        position = Position()
+        position.create_spatial_series(
+            name="TestName",
+            data=(position_online.xloc, position_online.yloc, position_online.xloc2, position_online.yloc2),
+            # What should be inside this field?
+            reference_frame="Description defining what the zero-position is",
+            timestamps=position_online.time.tolist()
+        )
+        self.assertEqual(position_online.shape, (32658, 5), 'Shape should be (32658, 5)')
