5e96974a82832a762ecbfffbd7c41d81413e5e46
MarcinAtNovela <55557514+MarcinAtNovela@users.noreply.github.com>
2019-10-31 13:46:30 +0100

    add mda int test (#21)

---
+++ src/e2etests/integration/experiment_data.py
@@ -0,0 +1,4 @@
+class ExperimentData:
+    animal_name = 'donald'
+    mda_folder = 'preprocessing/20190718/20190718_beans_01_s1.mda'
+    root_path = '../test_data/' + animal_name + '/' + mda_folder + '/'
