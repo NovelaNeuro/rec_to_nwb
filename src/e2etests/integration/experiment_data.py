eb444980c943b3a5b24b03fe5377f34c3ca4a0d6
acwikla-novela <55695978+acwikla-novela@users.noreply.github.com>
2019-10-31 16:19:25 +0100

    Add rec to bin generations test

--- src/e2etests/integration/experiment_data.py
+++ src/e2etests/integration/experiment_data.py
@@ -3,4 +3,6 @@ class ExperimentData:
     mda_folder = 'preprocessing/20190718/20190718_beans_01_s1.mda'
     pos_folder = '20190718_beans_01_s1.1.pos/'
     pos_file = '20190718_beans_01_s1.1.pos_online.dat'
-    root_path = '../test_data/' + animal_name + '/' + 'preprocessing/20190718/'
+    root_path = '../test_data/'
+    raw_root_path = '../test_data/' + animal_name + '/raw/'
+    preprocessing_root_path = '../test_data/' + animal_name + '/preprocessing/'
