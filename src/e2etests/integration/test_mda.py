bf1ec04da716f724392fb02876895b0dcb5b0411
acwikla-novela <55695978+acwikla-novela@users.noreply.github.com>
2019-10-31 16:34:24 +0100

    Add get all .mda files to list Minor changes in ExperimentData (like in previous PR)

--- src/e2etests/integration/test_mda.py
+++ src/e2etests/integration/test_mda.py
@@ -1,3 +1,4 @@
+import os
 import unittest
 
 from mountainlab_pytools.mdaio import readmda
@@ -9,10 +10,16 @@ class TestMDAMigration(unittest.TestCase):
 
     def setUp(self):
         print('Test requires preprocessed test_data folder at e2etests location')
-        self.timestamps = readmda(ExperimentData.root_path + '20190718_beans_01_s1.nt1.mda')
-        self.ntrode = readmda(ExperimentData.root_path + '20190718_beans_01_s1.timestamps.mda')
+        self.timestamps = readmda(ExperimentData.mda_path + ExperimentData.mda_file)
+        self.ntrode = readmda(ExperimentData.mda_path + ExperimentData.mda_timestamp)
 
-    def test_reading_mda(self):
+        timestamp_mda = '20190718_beans_01_s1.timestamps.mda'
+
+        mda_files = [mda_file for mda_file in os.listdir(ExperimentData.root_path + ExperimentData.mda_file) if
+                     (mda_file.endswith('.mda') and mda_file != timestamp_mda)]
+
+
+def test_reading_mda(self):
         self.assertIsNotNone(self.timestamps)
         self.assertIsNotNone(self.ntrode)
         self.assertEqual(self.ntrode.shape, (21839001,), 'Should be (21839001,)')
