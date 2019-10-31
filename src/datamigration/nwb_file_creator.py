67b84bb290371a23d72481afbf6b06833725d05f
Wojciech Merynda <48327847+WojciechMerynda@users.noreply.github.com>
2019-10-31 13:44:20 +0100

    changes to nwb file creator

--- src/datamigration/nwb_file_creator.py
+++ src/datamigration/nwb_file_creator.py
@@ -1,4 +1,5 @@
 from pynwb import NWBHDF5IO, NWBFile
+from pynwb.file import Subject
 
 
 class NWBFileCreator:
@@ -23,17 +24,23 @@ class NWBFileCreator:
         self.task = None
         self.electrode_locations = None
         self.recording_device = None
+        self.subject = None
+        self.electrodes = None
 
     def with_task(self, task):
         self.task = task
         return self
 
-    def with_electrode_locations(self, electrode_locations):
-        self.electrode_locations = electrode_locations
+    def with_electrode_locations(self, electrodes):
+        self.electrodes = electrodes
         return self
 
     def with_recording_device(self, recording_device):
         self.recording_device = recording_device
+        return self
+
+    def with_subject(self, age, description, genotype, sex, species, subject_id, weight, date_of_borth):
+        self.subject = Subject(age, description, genotype, sex, species, subject_id, weight, date_of_borth)
         return self
 
     def build(self):
@@ -44,8 +51,10 @@ class NWBFileCreator:
                           session_start_time=self.session_start_time,
                           identifier=self.identifier,
                           experiment_description=self.experiment_description,
+                          subject=self.subject,
                           )
 
         with NWBHDF5IO('example_file_path.nwb', mode='w') as nwb_file:
             nwb_file.write(nwbfile)
+
 
