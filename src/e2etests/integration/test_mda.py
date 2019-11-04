1baa536c4851e4f5be3b553cb0200ec260a77fe5
acwikla-novela <55695978+acwikla-novela@users.noreply.github.com>
2019-11-04 14:14:59 +0100

    Minor fix in path and names

--- src/e2etests/integration/test_mda.py
+++ src/e2etests/integration/test_mda.py
@@ -1,26 +1,22 @@
 import os
 import unittest
 
-from mountainlab_pytools.mdaio import readmda
-
 from experiment_data import ExperimentData
+from mountainlab_pytools.mdaio import readmda
 
 
 class TestMDAMigration(unittest.TestCase):
 
     def setUp(self):
         print('Test requires preprocessed test_data folder at e2etests location')
-        self.timestamps = readmda(ExperimentData.mda_path + ExperimentData.mda_file)
-        self.ntrode = readmda(ExperimentData.mda_path + ExperimentData.mda_timestamp)
+        self.ntrode = readmda(ExperimentData.mda_path + ExperimentData.mda_file)
+        self.timestamps = readmda(ExperimentData.mda_path + ExperimentData.mda_timestamp)
 
-        timestamp_mda = '20190718_beans_01_s1.timestamps.mda'
+        mda_files = [mda_file for mda_file in os.listdir(ExperimentData.mda_path) if
+                     (mda_file.endswith('.mda') and mda_file != ExperimentData.mda_timestamp)]
 
-        mda_files = [mda_file for mda_file in os.listdir(ExperimentData.root_path + ExperimentData.mda_file) if
-                     (mda_file.endswith('.mda') and mda_file != timestamp_mda)]
-
-
-def test_reading_mda(self):
+    def test_reading_mda(self):
         self.assertIsNotNone(self.timestamps)
         self.assertIsNotNone(self.ntrode)
-        self.assertEqual(self.ntrode.shape, (21839001,), 'Should be (21839001,)')
-        self.assertEqual(self.timestamps.shape, (4, 21839001), 'Should be (4, 21839001)')
+        self.assertEqual(self.timestamps.shape, (21839001,), 'Should be (21839001,)')
+        self.assertEqual(self.ntrode.shape, (4, 21839001), 'Should be (4, 21839001)')
