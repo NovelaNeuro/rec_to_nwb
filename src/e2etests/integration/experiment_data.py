753e75b245820b1bc6d63c8bcb3d6d61f89c397b
acwikla-novela <55695978+acwikla-novela@users.noreply.github.com>
2019-11-08 13:53:08 +0100

    Extract header into preprocessing folder

--- src/e2etests/integration/experiment_data.py
+++ src/e2etests/integration/experiment_data.py
@@ -11,10 +11,18 @@ class ExperimentData:
     pos_folder = '20190718_beans_01_s1.1.pos/'
     pos_file = '20190718_beans_01_s1.1.pos_online.dat'
 
+    rec_file = '20190718_beans_01_s1.rec'
+
+    metadata_file = 'metadata.yml'
+
+    xml_file = 'fl_lab_header.xml'
+    xsd_path = '../../data/fl_lab_header.xsd'
+
     root_path = '../test_data/'
     raw_root_path = root_path + animal_name + raw
     preprocessing_root_path = root_path + animal_name + preprocessing
 
     mda_path = preprocessing_root_path + date + mda_folder
     pos_path = preprocessing_root_path + date + pos_folder
-    metadata_path = preprocessing_root_path + date + 'metadata.yml'
+    metadata_path = preprocessing_root_path + date + metadata_file
+    rec_path = raw_root_path + date + rec_file
--- src/e2etests/integration/test_rec_to_binaries_generations.py
+++ src/e2etests/integration/test_rec_to_binaries_generations.py
@@ -3,6 +3,7 @@ import unittest
 
 from rec_to_binaries import extract_trodes_rec_file
 
+from src.datamigration.xml_extractor import XMLExtractor
 from src.e2etests.integration.experiment_data import ExperimentData
 
 
@@ -15,3 +16,13 @@ class TestRecToBinGeneration(unittest.TestCase):
         extract_trodes_rec_file(ExperimentData.root_path, ExperimentData.animal_name, parallel_instances=4)
         assert os.path.isdir(ExperimentData.preprocessing_root_path) == 1
         assert os.path.isdir(ExperimentData.preprocessing_root_path + "20190718") == 1
+
+    def test_generation_xml(self):
+        xml_extractor = XMLExtractor(
+            rec_path=ExperimentData.rec_path,
+            xml_path=ExperimentData.preprocessing_root_path + ExperimentData.date + ExperimentData.xml_file,
+            xsd_path=ExperimentData.xsd_path
+        )
+        xml_extractor.extract_xml_from_rec_file()
+        assert os.path.exists(
+            ExperimentData.preprocessing_root_path + ExperimentData.date + ExperimentData.xml_file) == 1
