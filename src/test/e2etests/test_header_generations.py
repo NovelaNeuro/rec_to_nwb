a47652d298bf2eeb9c352cd56c0e62af63876739
acwikla-novela <55695978+acwikla-novela@users.noreply.github.com>
2019-11-12 11:39:54 +0100

    User/acwikla/test header generations (#42)

---
+++ src/test/e2etests/test_header_generations.py
@@ -0,0 +1,21 @@
+import os
+import unittest
+
+from src.datamigration.xml_extractor import XMLExtractor
+from .experiment_data import ExperimentData
+
+
+class TestHeaderGenerations(unittest.TestCase):
+
+    def setUp(self):
+        self.xml_extractor = XMLExtractor(
+            rec_path=ExperimentData.rec_path,
+            xml_path=ExperimentData.preprocessing_root_path + ExperimentData.date + ExperimentData.xml_file,
+            xsd_path=ExperimentData.xsd_path
+        )
+
+    def test_generation_xml(self):
+        self.xml_extractor.extract_xml_from_rec_file()
+        self.assertTrue(os.path.exists(
+            ExperimentData.preprocessing_root_path + ExperimentData.date + ExperimentData.xml_file
+        ))
