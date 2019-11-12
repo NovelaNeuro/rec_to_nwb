cd0c77099c0eabb3f2650934cc9748510f383114
Wojciech Merynda <wmerynda@novelaneuro.com>
2019-11-12 19:18:44 +0100

    skeleton of probe builder

--- src/data/nwb_extension_builder_for_probe.py
+++ src/data/nwb_extension_builder_for_probe.py
@@ -1,21 +1,16 @@
-from pynwb.spec import NWBDatasetSpec, NWBNamespaceBuilder, NWBAttributeSpec
+from pynwb.spec import NWBNamespaceBuilder, NWBGroupSpec, NWBAttributeSpec
 
-# create a builder for the namespace
-probe_builder = NWBNamespaceBuilder("Extension for storage probe information", "probe")
+ns_path = "mylab.namespace.yaml"
+ext_source = "mylab.extensions.yaml"
 
-# create extensions
-probe = NWBDatasetSpec('A custom NWB probe',
-                    name='probe',
-                    attribute=[
-                        NWBAttributeSpec('probe_prop', 'a value for pp', 'str'),
-                    ],
-                    shape=(None, None))
+ns_builder = NWBNamespaceBuilder('Extension for novela', "novela")
 
-# add the extension
-ext_source = 'my_novela_lab_specs.yaml'
-probe_builder.add_spec(ext_source, probe)
+ns_builder.include_type('ElectricalSeries', namespace='core')
 
+ext = NWBGroupSpec('a probe for novela',
+                   attributes=[NWBAttributeSpec('probe_id', 'the probe id', 'int'), NWBAttributeSpec('probe2_id', 'the probe2 id', 'int')],
+                   neurodata_type_inc='ElectricalSeries',
+                   neurodata_type_def='ProbeSeries')
 
-# save the namespace and extensions
-ns_path = 'my_novela_lab.namespace.yaml'
-probe_builder.export(ns_path)
+ns_builder.add_spec(ext_source, ext)
+ns_builder.export(ns_path)
