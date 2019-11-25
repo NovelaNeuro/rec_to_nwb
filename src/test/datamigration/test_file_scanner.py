f5fd09f1ff08e2c97e6db1ef1af952bc332c5a72
acwikla <acwikla@novelaneuro.com>
2019-11-25 19:57:07 +0100

    Fix errors with versions of data

--- src/test/datamigration/test_file_scanner.py
+++ src/test/datamigration/test_file_scanner.py
@@ -12,14 +12,14 @@ class TestFileScanner(unittest.TestCase):
     @unittest.skip("DOES NOT WORK!!!")
     def test_scanner(self):
         self.assertEqual(ExperimentData.pos_path,
-                         self.data_folder.data['jaq']['20190911']['01_s1'].get_data_path_from_dataset('pos'))
-        self.assertEqual(len(self.data_folder.data['jaq']['20190911']['01_s1'].get_all_data_from_dataset('mda')),
+                         self.data_folder.data['beans']['20190718']['01_s1'].get_data_path_from_dataset('pos'))
+        self.assertEqual(len(self.data_folder.data['beans']['20190718']['01_s1'].get_all_data_from_dataset('mda')),
                          len(os.listdir(ExperimentData.mda_path)))
         self.assertEqual(ExperimentData.mda_path,
-                         self.data_folder.data['jaq']['20190911']['01_s1'].get_data_path_from_dataset('mda'))
-        self.assertEqual(len(self.data_folder.data['jaq']['20190911']['01_s1'].get_all_data_from_dataset('metadata')),
+                         self.data_folder.data['beans']['20190718']['01_s1'].get_data_path_from_dataset('mda'))
+        self.assertEqual(len(self.data_folder.data['beans']['20190718']['01_s1'].get_all_data_from_dataset('metadata')),
                          1)
 
-        # self.assertEqual(['jaq'], self.data_folder.get_all_animals())
-        # self.assertEqual(['20190911'], self.data_folder.get_all_experiment_dates('jaq'))
-        # self.assertEqual(['01_s1'], self.data_folder.get_all_datasets('jaq', '20190911'))
+        # self.assertEqual(['beans'], self.data_folder.get_all_animals())
+        # self.assertEqual(['20190718'], self.data_folder.get_all_experiment_dates('beans'))
+        # self.assertEqual(['01_s1'], self.data_folder.get_all_datasets('beans', '20190718'))
