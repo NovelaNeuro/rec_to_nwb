71cf492f2f9856b6c04a180091d21d64e57cbfae
Wojciech Merynda <wmerynda@novelaneuro.com>
2019-11-26 16:39:32 +0100

    fix travis

--- src/test/datamigration/test_extensions.py
+++ src/test/datamigration/test_extensions.py
@@ -14,7 +14,7 @@ class TestExtensions(TestCase):
 
         self.nwb_file = NWBFile(session_description='NWBFile extensions test',
                                 identifier='NWB123',
-                                session_start_time=datetime(2017, 4, 3, 11))
+                                session_start_time=datetime(2017, 4, 3, 11, 24, 48, 27631))
 
         self.probe = Probe(name='Probe1', probe_id='1')
         self.nwb_file.add_device(self.probe)
