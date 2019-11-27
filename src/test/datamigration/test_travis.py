7c1dde94f64e9ff1be195e3895776672cac4ad70
acwikla <acwikla@novelaneuro.com>
2019-11-27 12:22:05 +0100

    Travis test

--- src/test/datamigration/test_travis.py
+++ src/test/datamigration/test_travis.py
@@ -2,7 +2,6 @@ import unittest
 from datetime import datetime
 
 from dateutil.tz import tzlocal
-from pynwb import NWBFile
 
 start_time = datetime(2017, 4, 3, tzinfo=tzlocal())
 create_date = datetime(2017, 4, 12, tzinfo=tzlocal())
@@ -11,11 +10,15 @@ create_date = datetime(2017, 4, 12, tzinfo=tzlocal())
 class TestTravis(unittest.TestCase):
 
     def setUp(self):
-        self.nwb_file = NWBFile(session_description='demonstrate external files',
-                           identifier='NWBE1',
-                           session_start_time=start_time,
-                           file_create_date=create_date)
+        # self.nwb_file = NWBFile(session_description='demonstrate external files',
+        #                    identifier='NWBE1',
+        #                    session_start_time=start_time,
+        #                    file_create_date=create_date)
+        # self.assertEqual()
+        pass
+
 
 
     def test_travis(self):
-       self.assertIsNotNone(self.nwb_file)
+        # self.assertIsNotNone(self.nwb_file)
+        self.assertIsInstance(start_time, datetime)
