e9ca093659ee92d4d8d79c9e69e24c1e1c904b4b
Wojciech Merynda <wmerynda@novelaneuro.com>
2019-11-27 17:37:18 +0100

    missing call od super __init__ fix

--- src/datamigration/extension/probe.py
+++ src/datamigration/extension/probe.py
@@ -15,5 +15,8 @@ class Probe(Device):
     @docval(*get_docval(Device.__init__) + (
             {'name': 'probe_id', 'type': 'str', 'doc': 'unique id of the probe'},))
     def __init__(self, **kwargs):
+        device_kwargs = kwargs.copy()
+        del(device_kwargs['probe_id'])
+        super().__init__(**device_kwargs)
         call_docval_func(super(Probe, self).__init__, kwargs)
         self.probe_id = kwargs['probe_id']
