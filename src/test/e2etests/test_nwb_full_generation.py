073316215e81bd912ec528deef3ebd094d2e57ad
MarcinAtNovela <55557514+MarcinAtNovela@users.noreply.github.com>
2019-11-28 18:13:23 +0100

    Cleanup

---
+++ src/test/e2etests/test_nwb_full_generation.py
@@ -0,0 +1,25 @@
+import unittest
+
+from src.datamigration.nwb_file_builder import NWBFileBuilder
+from .experiment_data import ExperimentData
+
+
+class TestNwbFullGeneration(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        self.nwbBuilder = NWBFileBuilder(
+            data_path=ExperimentData.root_path,
+            animal_name='beans',
+            date='20190718',
+            dataset='01_s1',
+            config_path='datamigration/res/metadata.yml',
+            header_path='datamigration/res/fl_lab_sample_header.xml')
+
+    def test_generate_nwb(self):
+        self.nwbBuilder.build()
+        self.assertIsNotNone(self.nwbBuilder)
+
+    @classmethod
+    def tearDownClass(cls):
+        del cls.nwbBuilder
