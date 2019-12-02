f3e8054d3508552cd4f2b125499015c9b7e529d7
Wojciech Merynda <wmerynda@novelaneuro.com>
2019-12-02 13:56:25 +0100

    write to txt

--- src/datamigration/nwb_builder/dio_extractor.py
+++ src/datamigration/nwb_builder/dio_extractor.py
@@ -1,26 +1,42 @@
+import datetime
 import os
 
+import numpy as np
+import pandas as pd
+import pynwb
+from dateutil.tz import tzlocal
+from pynwb import NWBFile, NWBHDF5IO, TimeSeries
+from pynwb.behavior import BehavioralEvents
 from rec_to_binaries.read_binaries import readTrodesExtractedDataFile
 
-log = open('log.txt', 'w')
-
-for index in range(32):
-    index += 1
-    d_in = readTrodesExtractedDataFile('C:/Users/wmery/PycharmProjects/LorenFranksDataMigration/src/test/test_data/jaq/preprocessing/20190911/20190911_jaq_01_s1.DIO/20190911_jaq_01_s1.dio_Din'+str(index)+'.dat')
-    log.write(str(d_in))
-    log.write('\n')
-    log.write(str(index))
-
-
-# class DioExtractor:
+d_in = readTrodesExtractedDataFile(
+    'C:/Users/wmery/PycharmProjects/LorenFranksDataMigration/src/test/test_data/jaq/preprocessing/20190911/20190911_jaq_01_s1.DIO/20190911_jaq_01_s1.dio_Din1.dat')
+d_in1 = pd.DataFrame(d_in['data'])
+print(d_in1)
+# st = datetime.datetime(20, 5, 5, tzinfo=tzlocal())
 #
-#     def __init__(self, path):
-#         self.path = path
-#         pass
+# def build(session_start_time, dio):
 #
-#     def get_dio(self):
-#         dio_names = [dio_file for dio_file in os.listdir(self.path) if
-#                      (dio_file.endswith('.dio') and not dio_file.endswith('timestamps.dio'))]
-#         dio_files = []
-#         for dio_file in dio_names:
-#             dio_files.append(self.path + dio_file)
+#     content = NWBFile(session_description='self.metadata.session_description',
+#                                    experimenter='self.metadata.experimenter_name',
+#                                    lab='self.metadata.lab',
+#                                    institution='self.metadata.institution',
+#                                    session_start_time=session_start_time,
+#                                    identifier=str('self.metadata.identifier'),
+#                                    experiment_description='self.metadata.experiment_description'
+#                                    )
+#
+#     ts = TimeSeries(name='behavioral_events_name', source='behavioral_events_source',
+#                     data=dio['data'],
+#                     timestamps=dio['timestamp_at_creation'],
+#                     )
+#
+#     behavioral_events = BehavioralEvents(time_series=ts, name='BehavioralEvents_name')
+#     behavioral_events.add_timeseries(ts)
+#
+#
+#     return content
+#
+# with NWBHDF5IO('output_file', mode='w') as nwb_fileIO:
+#     nwb_fileIO.write(build(st, d_in))
+#     nwb_fileIO.close()
