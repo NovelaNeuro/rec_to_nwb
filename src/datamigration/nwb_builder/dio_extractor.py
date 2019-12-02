6bf27a395b4a00e82e90538ed17f72e1eeab4fd9
Wojciech Merynda <wmerynda@novelaneuro.com>
2019-12-02 16:20:16 +0100

    working write to nwb

--- src/datamigration/nwb_builder/dio_extractor.py
+++ src/datamigration/nwb_builder/dio_extractor.py
@@ -9,10 +9,22 @@ from pynwb import NWBFile, NWBHDF5IO, TimeSeries
 from pynwb.behavior import BehavioralEvents
 from rec_to_binaries.read_binaries import readTrodesExtractedDataFile
 
-d_in = readTrodesExtractedDataFile(
-    'C:/Users/wmery/PycharmProjects/LorenFranksDataMigration/src/test/test_data/jaq/preprocessing/20190911/20190911_jaq_01_s1.DIO/20190911_jaq_01_s1.dio_Din1.dat')
-d_in1 = pd.DataFrame(d_in['data'])
-print(d_in1)
+timestamp_list = []
+pulses = []
+for index in range(1, 33):
+    d_in = readTrodesExtractedDataFile(
+        'C:/Users/wmery/PycharmProjects/LorenFranksDataMigration/src/test/test_data/jaq/preprocessing/20190911/20190911_jaq_01_s1.DIO/20190911_jaq_01_s1.dio_Din' + str(index) + '.dat')
+    for event_register in d_in['data']:
+        timestamp_list.append(event_register[0])
+        pulses.append(event_register[1])
+
+print(timestamp_list)
+print(pulses)
+
+
+def add_behavioral_events(behavioral_timestamp, behavioral_data):
+    return BehavioralEvents(name='behavioral name', time_series=TimeSeries(name='behavioral timeseries', data=behavioral_data, timestamps=behavioral_timestamp))
+
 
 
 def build():
@@ -27,10 +39,18 @@ def build():
                                    experiment_description='self.metadata.experiment_description'
                                    )
 
-
+    content.create_processing_module(
+        name='behaviour',
+        description='Sample behaviour description'
+    ).add_data_interface(
+        add_behavioral_events(timestamp_list, pulses)
+    )
 
     return content
 
 with NWBHDF5IO('output_file.nwb', mode='w') as nwb_fileIO:
     nwb_fileIO.write(build())
-    nwb_fileIO.close()
+
+
+with NWBHDF5IO('output_file.nwb', mode='r') as nwb_fileIO:
+    print((nwb_fileIO.read().processing['behaviour']['behavioral name'].get_timeseries('behavioral timeseries').data))
