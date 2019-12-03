0fe769ac9759ebc546e623f511d48f07338ea67a
wbodo <55714242+wbodo@users.noreply.github.com>
2019-12-03 11:27:13 +0100

    code cleanup for file sorted and adding new test cases to test

--- src/datamigration/file_sorter.py
+++ src/datamigration/file_sorter.py
@@ -1,18 +1,10 @@
 class FileSorter:
     def sort_filenames(self, filenames):
-        max_filename_length = len(filenames[0])
-        min_filename_length = len(filenames[0])
-        for filename in filenames:
-            if len(filename) > max_filename_length:
-                max_filename_length = len(filename)
-            elif len(filename) < min_filename_length:
-                min_filename_length = len(filename)
+        max_filename_length = len(max(filenames, key=len))
+        min_filename_length = len(min(filenames, key=len))
         sorted_files = []
         for i in range(min_filename_length, max_filename_length + 1):
-            temp_data = []
-            for filename in filenames:
-                if len(filename) == i:
-                    temp_data.append(filename)
+            temp_data = [file for file in filenames if len(file) == i]
             temp_data.sort()
             sorted_files = sorted_files + temp_data
         return sorted_files
