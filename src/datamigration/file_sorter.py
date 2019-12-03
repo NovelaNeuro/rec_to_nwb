05ed8bfe354f102719f2c010d14e157fb393cf98
wbodo <55714242+wbodo@users.noreply.github.com>
2019-12-03 18:01:20 +0100

    code cleanup

--- src/datamigration/file_sorter.py
+++ src/datamigration/file_sorter.py
@@ -1,10 +1,4 @@
 class FileSorter:
     def sort_filenames(self, filenames):
-        max_filename_length = len(max(filenames, key=len))
-        min_filename_length = len(min(filenames, key=len))
-        sorted_files = []
-        for i in range(min_filename_length, max_filename_length + 1):
-            temp_data = [file for file in filenames if len(file) == i]
-            temp_data.sort()
-            sorted_files = sorted_files + temp_data
-        return sorted_files
+        filenames.sort(key=lambda item: (len(item), item))
+        return filenames
