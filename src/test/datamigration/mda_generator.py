51a18903de9563118bfd95cf15d42dca32260a62
wbodo <55714242+wbodo@users.noreply.github.com>
2019-12-04 12:41:34 +0100

    path update

--- src/test/datamigration/mda_generator.py
+++ src/test/datamigration/mda_generator.py
@@ -6,5 +6,5 @@ for i in range(1, 4):
             [i, i, i],
             [i, i, i]]
     arr = np.array(data, 'int16')
-    name = '20190718_beans_01_s1.nt' + str(i) + '.mda'
+    name = 'res/20190718_beans_01_s1.nt' + str(i) + '.mda'
     writemda(X=arr, fname=name, dtype='int16')
