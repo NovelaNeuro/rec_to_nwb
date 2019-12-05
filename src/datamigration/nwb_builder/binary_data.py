e157a83dd6c22701f14739052629ca5b62f66223
wbodo <55714242+wbodo@users.noreply.github.com>
2019-12-05 15:37:34 +0100

    update data_iterator

--- src/datamigration/nwb_builder/binary_data.py
+++ src/datamigration/nwb_builder/binary_data.py
@@ -1,17 +1,41 @@
 import numpy as np
-
+from mountainlab_pytools.mdaio import readmda
 
 class BinaryData:
-    def __init__(self, directories, data_type):
+    def __init__(self, directories):
         self.directories = directories
         self.num_datasets = np.size(directories, 0)
+        self.single_dataset_len = np.size(directories, 1)
         self.num_rows_per_file = self.get_data_shape(0)[0]
-        self.data_len = [self.get_data_shape(j, i)[1]
-                         for i in np.size(directories, 1) for j in range(self.num_datasets)]
-        self.data_type = data_type
+        self.file_lenghts = [self.get_data_shape(i)[1] for i in range(self.num_datasets)]
+
+    def get_final_data_shape(self):
+        return [self.num_rows_per_file * self.single_dataset_len, sum(self.file_lenghts)]
+
 
     def read_data(self, dataset_num, file_num):
-        return  # to be overloaded by inheriting classes
+        return [0]  # to be overloaded by inheriting classes
 
-    def get_data_shape(self, num_of_dim):
-        return 0  # to be overloaded by inheriting classes
+    def get_data_shape(self, dataset_num):
+        dim1 = np.size(self.read_data(dataset_num, 0), 0)
+        dim2 = np.size(self.read_data(dataset_num, 0), 1)
+        return dim1, dim2
+
+
+class MdaData(BinaryData):
+    def read_data(self, dataset_num, file_num):
+        return readmda(self.directories[dataset_num][file_num])
+
+
+if __name__ == "__main__":
+    path = 'C:/Users/wbodo/Documents/GitHub/LorenFranksDataMigration/src/test/datamigration/res/'
+    directories = []
+    dataset1 = []
+    dataset2 = []
+    for i in range(1, 4):
+        dataset1.append(path + '20190718_beans_01_s1.nt' + str(i) + '.mda')
+        dataset2.append(path + '20190718_beans_01_s1.nt' + str(i) + '.mda')
+    directories.append(dataset1)
+    directories.append(dataset2)
+    mdadata = MdaData(directories)
+    print(mdadata.get_final_data_shape())
--- src/datamigration/nwb_builder/data_iterator.py
+++ src/datamigration/nwb_builder/data_iterator.py
@@ -1,25 +1,26 @@
 import numpy as np
 from hdmf.data_utils import AbstractDataChunkIterator, DataChunk
-from mountainlab_pytools.mdaio import readmda
+
 
 class DataIterator(AbstractDataChunkIterator):
 
-    def __init__(self, channel_files, num_steps, num_rows, num_datasets):
-        self.shape = (num_rows * 64, num_steps * num_datasets)
-        self.channel_files = channel_files
-        self.num_steps = num_steps
-        self.num_datasets = num_datasets
+    def __init__(self, data):
+        self.data = data
+        self.shape = data.get_final_data_shape()
+        self.files = data.directories
+        self.num_steps = data.num_datasets * data.single_dataset_len
         self.__curr_index = 0
         self.current_file = 0
         self.current_dataset = 0
-        self.num_rows = num_rows
+        self.num_rows = data.num_rows_per_file
+        self.num_files_in_single_dataset = data.single_dataset_len
 
     def __iter__(self):
         return self
 
     def __next__(self):
-        if self.__curr_index < len(self.channel_files):
-            new_data = readmda(self.channel_files[self.__curr_index])
+        if self.__curr_index < self.num_steps:
+            new_data = self.data.read_data(self.current_dataset, self.current_file)
             chunk = DataChunk(data=new_data,
                               selection=np.s_[((self.current_file) * self.num_rows):
                                               ((((self.current_file) + 1) * self.num_rows)),
@@ -28,7 +29,7 @@ class DataIterator(AbstractDataChunkIterator):
                                          self.num_steps)])
             self.__curr_index += 1
             self.current_file += 1
-            if self.current_file >= 64:
+            if self.current_file >= self.num_files_in_single_dataset:
                 self.current_dataset += 1
                 self.current_file = 0
             del new_data
@@ -50,3 +51,4 @@ class DataIterator(AbstractDataChunkIterator):
     @property
     def maxshape(self):
         return self.shape
+
