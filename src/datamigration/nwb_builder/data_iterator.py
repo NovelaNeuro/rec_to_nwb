255ed0147f4ac107deebbb7781c64a12705361d3
wbodo <55714242+wbodo@users.noreply.github.com>
2019-12-11 16:18:05 +0100

    data iterator cleanup

--- src/datamigration/nwb_builder/data_iterator.py
+++ src/datamigration/nwb_builder/data_iterator.py
@@ -53,22 +53,16 @@ class DataIterator(AbstractDataChunkIterator):
         return self.shape
 
 
-class DataIterator1D(AbstractDataChunkIterator):
+class DataIterator1D(DataIterator):
 
     def __init__(self, data):
         self.data = data
         self.files = data.directories
         self.num_steps = data.num_datasets
         self.__curr_index = 0
-        self.current_file = 0
         self.current_dataset = 0
         self.dataset_file_lenght = data.file_lenghts
-        self.num_rows = 1
-        self.num_files_in_single_dataset = 1
         self.shape = data.get_final_data_shape()
-
-    def __iter__(self):
-        return self
 
     def __next__(self):
         if self.__curr_index < self.num_steps:
@@ -77,26 +71,8 @@ class DataIterator1D(AbstractDataChunkIterator):
                               selection=np.s_[sum(self.dataset_file_lenght[0:self.current_dataset]):
                                               sum(self.dataset_file_lenght[0:self.current_dataset + 1]), ])
             self.__curr_index += 1
-            self.current_file += 1
-            if self.current_file >= self.num_files_in_single_dataset:
-                self.current_dataset += 1
-                self.current_file = 0
             del new_data
             return chunk
         raise StopIteration
 
     next = __next__
-
-    def recommended_chunk_shape(self):
-        return None  # Use autochunking
-
-    def recommended_data_shape(self):
-        return self.shape
-
-    @property
-    def dtype(self):
-        return np.dtype('int16')
-
-    @property
-    def maxshape(self):
-        return self.shape
