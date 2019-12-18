f34c3e010afb425d310b544042368a700ecb210e
Wojciech Merynda <wmerynda@novelaneuro.com>
2019-12-18 12:05:49 +0100

    replace test header reader

--- src/test/e2etests/test_header_reader.py
+++ src/test/e2etests/test_header_reader.py
@@ -6,19 +6,19 @@ from src.datamigration.nwb_builder.header_checker.header_reader import HeaderRea
 path = os.path.dirname(os.path.abspath(__file__))
 
 
-@unittest.skip('local test need to be launched with e2etests')
+
 class TestHeaderReader(unittest.TestCase):
 
     def setUp(self):
-        with open(path + '/res/test_xmls/test1.xml', 'a+') as xml_1:
+        with open(path + '/../datamigration/res/test_xmls/test1.xml', 'a+') as xml_1:
             xml_1.write('string string string')
-        with open(path + '/res/test_xmls/test2.xml', 'a+') as xml_2:
+        with open(path + '/../datamigration//res/test_xmls/test2.xml', 'a+') as xml_2:
             xml_2.write('random test strings')
-        with open(path + '/res/test_xmls/test3.xml', 'a+') as xml_3:
+        with open(path + '/../datamigration//res/test_xmls/test3.xml', 'a+') as xml_3:
             xml_3.write('some content')
-        self.header_reader = HeaderReader([path + '/res/test_xmls/test1.xml',
-                                           path + '/res/test_xmls/test2.xml',
-                                           path + '/res/test_xmls/test3.xml']
+        self.header_reader = HeaderReader([path + '/../datamigration/res/test_xmls/test1.xml',
+                                           path + '/../datamigration/res/test_xmls/test2.xml',
+                                           path + '/../datamigration/res/test_xmls/test3.xml']
                                           )
 
     def test_reading_headers(self):
