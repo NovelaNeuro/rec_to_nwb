d98220beb380712bc0999e2b16d39a9e91dbe8e0
Wojciech Merynda <48327847+WojciechMerynda@users.noreply.github.com>
2019-12-20 12:36:07 +0100

    User/wmerynda/add dio building and clean code (#87)

--- src/datamigration/nwb_builder/header_checker/header_reader.py
+++ src/datamigration/nwb_builder/header_checker/header_reader.py
@@ -1,4 +1,5 @@
 import os
+from xmldiff import main
 
 
 class HeaderReader:
@@ -6,6 +7,10 @@ class HeaderReader:
     def __init__(self, xml_files):
         self.xml_headers = []
         self.xml_files = xml_files
+        self.headers_differences = []
+        first_xml_file = xml_files[0]
+        for xml_file in xml_files:
+            self.headers_differences += main.diff_files(first_xml_file, xml_file)
 
     def read_headers(self):
         for xml_file in self.xml_files:
