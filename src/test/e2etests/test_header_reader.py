6a86ba6f1075d916c9fd6511ea7d7dadf8428985
Wojciech Merynda <wmerynda@novelaneuro.com>
2019-12-23 13:18:16 +0100

    change test header reader

--- src/test/e2etests/test_header_reader.py
+++ src/test/e2etests/test_header_reader.py
@@ -5,16 +5,17 @@ from src.datamigration.nwb_builder.header_checker.header_reader import HeaderRea
 
 path = os.path.dirname(os.path.abspath(__file__))
 
+
 @unittest.skip('test need to create files localy not working on travis')
 class MyTestCase(unittest.TestCase):
 
     def setUp(self):
-        with open(path + '/../datamigration/res/test_xmls/test1.xml', 'a+') as xml_1:
-            xml_1.write('string string string')
-        with open(path + '/../datamigration//res/test_xmls/test2.xml', 'a+') as xml_2:
-            xml_2.write('random test strings')
-        with open(path + '/../datamigration//res/test_xmls/test3.xml', 'a+') as xml_3:
-            xml_3.write('some content')
+        with open(path + '/../datamigration/res/test_xmls/test1.xml', 'w') as xml_1:
+            xml_1.write('<string_string_string/>')
+        with open(path + '/../datamigration//res/test_xmls/test2.xml', 'w') as xml_2:
+            xml_2.write('<random_test_strings/>')
+        with open(path + '/../datamigration//res/test_xmls/test3.xml', 'w') as xml_3:
+            xml_3.write('<some_content/>')
         self.header_reader = HeaderReader([path + '/../datamigration/res/test_xmls/test1.xml',
                                            path + '/../datamigration/res/test_xmls/test2.xml',
                                            path + '/../datamigration/res/test_xmls/test3.xml']
@@ -26,7 +27,7 @@ class MyTestCase(unittest.TestCase):
         self.assertIsNotNone(headers_content[2])
         self.assertNotEqual('', headers_content[0])
         self.assertEqual(3, len(headers_content))
-        self.assertEqual('some content', headers_content[2])
-        self.assertEqual('random test strings', headers_content[1])
-        self.assertEqual('string string string', headers_content[0])
+        self.assertEqual('<some_content/>', headers_content[2])
+        self.assertEqual('<random_test_strings/>', headers_content[1])
+        self.assertEqual('<string_string_string/>', headers_content[0])
 
