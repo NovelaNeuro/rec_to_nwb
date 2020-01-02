a58778c8d2b73baa46fb5adcb0cb6cf1e9030b0c
Wojciech Merynda <wmerynda@novelaneuro.com>
2020-01-02 12:40:18 +0100

    basic structure of probe objects

--- src/datamigration/probe/probe.py
+++ src/datamigration/probe/probe.py
@@ -8,5 +8,5 @@ class Probe:
     def __init__(self, probe_yml_path):
         with open(probe_yml_path, 'r') as stream:
             self.probe_content = yaml.safe_load(stream)
-            self.electrodes = [Electrode(electrode) for electrode in self.probe_content['shanks'].values(0)]
+            self.electrodes = [Electrode(electrode) for electrode in self.probe_content['shanks']]
 
--- src/test/datamigration/test_probe_extractor.py
+++ src/test/datamigration/test_probe_extractor.py
@@ -9,11 +9,11 @@ path = os.path.dirname(os.path.abspath(__file__))
 class TestProbeExtractor(unittest.TestCase):
 
     def setUp(self):
-        self.probes = [ProbeExtractor(probe_name) for probe_name
+        self.probes = [ProbeExtractor(probe_name).probe for probe_name
                   in [path+'/res/probe.yml', path+'/res/probe2.yml', path+'/res/probe3.yml']
                   ]
 
     def test_probes(self):
         self.assertEqual(3, len(self.probes))
         self.assertEqual('tetrode_12.5', self.probes[0].probe_type)
-        self.assertEqual(32, len(self.probes[1].shanks))
+        self.assertEqual(32, len(self.probes[1].electrodes))
