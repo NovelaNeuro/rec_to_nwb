f141fd0cd99b517777a6e23d6badb0d69cf6c5e8
wbodo <55714242+wbodo@users.noreply.github.com>
2020-01-07 16:46:26 +0100

    adding test to testing get_probes

--- src/test/datamigration/test_file_scanner.py
+++ src/test/datamigration/test_file_scanner.py
@@ -11,6 +11,7 @@ class TestFileScanner(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         cls.data_folder = fs.DataScanner(path + '/res/scanner_test/')
+        cls.probe_path = path + '/res/probe_test'
 
     def test_finding_all_datasets(self):
         self.assertEqual(len(self.data_folder.data.values()), 1)
@@ -50,3 +51,10 @@ class TestFileScanner(unittest.TestCase):
                          3)
         self.assertEqual(len(self.data_folder.data['alien']['21251015']['01_s1'].get_all_data_from_dataset('time')),
                          2)
+
+    def test_get_probes(self):
+        probes = self.data_folder.get_probes_from_directory(self.probe_path)
+        probes.sort()
+        self.assertEqual('probe1.yml', probes[0], 'should be probe1.yml')
+        self.assertEqual('probe2.yml', probes[1], 'should be probe2.yml')
+        self.assertEqual('probe21.yml', probes[2], 'should be probe21.yml')
