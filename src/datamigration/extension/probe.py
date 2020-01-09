4b8c00557c73ea094f63ff77752c2a8989e8fe37
Wojciech Merynda <wmerynda@novelaneuro.com>
2020-01-09 14:10:58 +0100

    new probe definition and format probes.yml

--- src/datamigration/extension/probe.py
+++ src/datamigration/extension/probe.py
@@ -1,27 +1,33 @@
-import os
-
 from hdmf.utils import docval, call_docval_func, get_docval
-from pynwb import register_class, load_namespaces
-from pynwb.device import Device
-
-ns_path = os.path.dirname(os.path.abspath(__file__)) + "/NovelaNeurotechnologies.namespace.yaml"
-load_namespaces(ns_path)
+from pynwb.ecephys import ElectrodeGroup
 
 
-@register_class('Probe', 'NovelaNeurotechnologies')
-class Probe(Device):
-    __nwbfields__ = ('device_name', 'probe_description')
+class Probe(ElectrodeGroup):
+    __nwbfields__ = ('id', 'ntrodes', 'electrode_groups', 'num_shanks', 'contact_size', 'probe_type')
 
-    @docval(*get_docval(Device.__init__) + (
-            {'name': 'device_name', 'type': 'str', 'doc': 'name of the device'},
-            {'name': 'probe_description', 'type': 'str', 'doc': 'description of the probe'},
+    @docval(*get_docval(ElectrodeGroup.__init__) + (
+            {'name': 'id', 'type': 'int', 'doc': 'id of probe'},
+            {'name': 'ntrodes', 'type': 'list', 'doc': 'ntrodes list'},
+            {'name': 'electrode_groups', 'type': 'list', 'doc': 'list of electrode groups'},
+            {'name': 'num_shanks', 'type': 'int', 'doc': 'number of shanks'},
+            {'name': 'contact_size', 'type': 'float', 'doc': 'contact size value'},
+            {'name': 'probe_type', 'type': 'str', 'doc': 'type of probe'},
+
     ))
     def __init__(self, **kwargs):
         super().__init__(**{kwargs_item: kwargs[kwargs_item]
                             for kwargs_item in kwargs.copy()
-                            if kwargs_item != 'device_name'
-                            if kwargs_item != 'probe_description'
+                            if kwargs_item != 'id'
+                            if kwargs_item != 'ntrodes'
+                            if kwargs_item != 'electrode_groups'
+                            if kwargs_item != 'num_shanks'
+                            if kwargs_item != 'contact_size'
+                            if kwargs_item != 'probe_type'
                             })
         call_docval_func(super(Probe, self).__init__, kwargs)
-        self.device_name = kwargs['device_name']
-        self.probe_description = kwargs['probe_description']
+        self.id = kwargs['id']
+        self.ntrodes = kwargs['ntrodes']
+        self.electrode_groups = kwargs['electrode_groups']
+        self.num_shanks = kwargs['num_shanks']
+        self.contact_size = kwargs['contact_size']
+        self.probe_type = kwargs['probe_type']
