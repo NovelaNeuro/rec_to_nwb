ebccd3b3b8033462fde927cb3ca068005300a480
wbodo <55714242+wbodo@users.noreply.github.com>
2020-01-13 22:48:40 +0100

    add electrode extractor

--- src/datamigration/extension/electrodes.py
+++ src/datamigration/extension/electrodes.py
@@ -1,10 +1,15 @@
-class Electrodes():
-    def __init__(self, nwb_file_content, metadata):
-        self.nwb_file_content = nwb_file_content
-        self.metadata = metadata
+from src.datamigration.nwb_builder.electrode_extractor import ElectrodeExtractor
 
-    def get_data_from_metadata(self, property_name):
-        print("whtvr")
+
+class Electrodes():
+    def __init__(self, nwb_file_content, probe_files):
+        self.nwb_file_content = nwb_file_content
+        self.electrode_extractor = ElectrodeExtractor(probe_files)
+        self.electrodes = self.get_data_from_ymls()
+        self.add_electrodes()
+
+    def get_data_from_ymls(self):
+        return self.electrode_extractor.get_all_electrodes()
 
     def add_electrode_property(self, new_column_name,
                                data_for_existing_electrodes=[], column_description='No description'):
@@ -15,15 +20,25 @@ class Electrodes():
             data=data
         )
 
+    def add_all_electrode_properties(self):
+        base_properties = ["x", "y", "z", "imp", "location", "filtering", "electrode_group", "id"]
+        new_properties = []
+        for key in self.electrodes[0].keys:
+            if key not in base_properties:
+                new_properties.append(key)
+        for new_property in new_properties:
+            data = [electrode[new_property] for electrode in self.electrodes]
+            self.add_electrode_property(new_property, data)
+
     def add_electrodes(self):
-        for electrode in self.metadata.electrodes:
+        for electrode in self.electrodes:
             self.nwb_file_content.add_electrode(
-                x=electrode['x'],
-                y=electrode['y'],
-                z=electrode['z'],
+                x=0,
+                y=0,
+                z=0,
                 imp=1.0,
                 location='necessary location',
-                filtering=electrode['filtering'],
-                group=self.nwb_file_content.electrode_groups['1'],
-                id=electrode['id'],
+                filtering="have no idea",
+                group=electrode["electrode_group"],
+                id=electrode['id'],  # from header
             )
---
+++ src/datamigration/nwb_builder/electrode_extractor.py
@@ -0,0 +1,35 @@
+import yaml
+
+
+class ElectrodeExtractor:
+    def __init__(self, probes):
+        self.probes = probes
+
+    def get_all_electrodes_from_probe(self, probe_path):
+        electrodes = []
+        with open(probe_path, 'r') as stream:
+            probe_dict = yaml.safe_load(stream)
+            shanks = probe_dict["shanks"]
+            for shank in shanks:
+                electrodes_from_current_shank = shank["electrodes"]
+                for electrode in electrodes_from_current_shank:
+                    electrode["shank"] = shank["shank_num"]
+                    electrode["electrode_group"] = shank["electrode_group_name"]
+                electrodes = electrodes + electrodes_from_current_shank
+        for electrode in electrodes:
+            electrode["probe"] = probe_dict["id"]
+        return electrodes
+
+    def get_all_electrodes(self):
+        electrodes = []
+        for probe in self.probes:
+            electrodes = electrodes + self.get_all_electrodes_from_probe(probe)
+        return electrodes
+
+
+if __name__ == "__main__":
+    prb = "C:/Users/wbodo/Documents/GitHub/LorenFranksDataMigration/src/test/datamigration/res/probe3.yml"
+    prbs = [prb, prb]
+    ext = ElectrodeExtractor(prbs)
+    trodes = ext.get_all_electrodes()
+    print(trodes)
