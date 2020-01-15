2f2535009a95c2e73e58ccf038c7f0ffdcfaf659
wbodo <55714242+wbodo@users.noreply.github.com>
2020-01-15 09:13:54 +0100

    fixing test rec to binaries

--- src/test/e2etests/test_rec_to_binaries_generations.py
+++ src/test/e2etests/test_rec_to_binaries_generations.py
@@ -11,6 +11,6 @@ class TestRecToBinGeneration(unittest.TestCase):
 
     @unittest.skip("Super heavy REC to Preprocessing Generation")
     def test_generation_preprocessing(self):
-        extract_trodes_rec_file('testdata/', 'beans', parallel_instances=4)
-        self.assertTrue(os.path.isdir('testdata/beans/preprocessing'))
-        self.assertTrue(os.path.isdir('testdata/beans/preprocessing' + "20190718"))
+        extract_trodes_rec_file('test_data/', 'beans', parallel_instances=4)
+        self.assertTrue(os.path.isdir('test_data/beans/preprocessing'))
+        self.assertTrue(os.path.isdir('test_data/beans/preprocessing/' + "20190718"))
