0f982081d1598f82e27dbc83f811ae84087d9f7f
wbodo <55714242+wbodo@users.noreply.github.com>
2020-01-29 13:07:52 +0100

    adding new timestamps to pos

--- src/datamigration/nwb_builder/dio_extractor.py
+++ src/datamigration/nwb_builder/dio_extractor.py
@@ -4,12 +4,15 @@ from pynwb.base import TimeSeries
 from pynwb.behavior import BehavioralEvents
 from rec_to_binaries.read_binaries import readTrodesExtractedDataFile
 
+from src.datamigration.exceptions.missing_data_exception import MissingDataException
+
 
 class DioExtractor:
 
     def __init__(self, data_path, metadata):
         self.data_path = data_path
         self.dio_paths = [dio_set for dio_set in os.listdir(data_path) if dio_set.endswith('DIO')]
+        self.time_paths = [dio_set for dio_set in os.listdir(data_path) if dio_set.endswith('.time')]
         self.metadata = metadata
 
     def get_dio(self):
@@ -17,13 +20,23 @@ class DioExtractor:
         for dio_time_series in self.metadata['behavioral_events']:
             temp_timeseries = []
             temp_timestamps = []
-            for dio_set in self.dio_paths:
+            for i in range(len(self.dio_paths)):
+                dio_set = self.dio_paths[i]
+                time_set = self.time_paths[i]
+                continuous_time_file = None
+                for file in os.listdir(self.data_path + '/' + time_set):
+                    if file.endswith('continuoustime.dat'):
+                        continuous_time_file = self.data_path + '/' + time_set + "/" + file
+                if not continuous_time_file:
+                    raise MissingDataException("continuous time file not found")
+                continuous_time = readTrodesExtractedDataFile(continuous_time_file)
+                continuous_time_dict = {str(data[0]): float(data[1]) for data in continuous_time['data']}
                 for dio_file in os.listdir(self.data_path + '/' + dio_set):
                     if dio_time_series['name'] in dio_file:
                         dio_data = readTrodesExtractedDataFile(self.data_path + '/' + dio_set + '/' + dio_file)
                         for recorded_event in dio_data['data']:
                             temp_timeseries.append(recorded_event[1])
-                            temp_timestamps.append(recorded_event[0])
+                            temp_timestamps.append(continuous_time_dict[str(recorded_event[0])] / 1E9)
             behavioral_event.add_timeseries(time_series=TimeSeries(name=dio_time_series['name'],
                                                                    data=temp_timeseries,
                                                                    timestamps=temp_timestamps,
