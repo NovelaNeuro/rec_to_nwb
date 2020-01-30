0d43cdab0836c1c7574c032331af2b5356c66563
wbodo <55714242+wbodo@users.noreply.github.com>
2020-01-30 16:40:20 +0100

    split get_dio into many smaller functions

--- src/datamigration/nwb_builder/extractors/dio_extractor.py
+++ src/datamigration/nwb_builder/extractors/dio_extractor.py
@@ -14,17 +14,13 @@ class DioExtractor:
         self.dio_paths = [dio_set for dio_set in os.listdir(data_path) if dio_set.endswith('DIO')]
         self.time_paths = [dio_set for dio_set in os.listdir(data_path) if dio_set.endswith('.time')]
         self.metadata = metadata
+        self.all_dio_timeseries = metadata['behavioral_events']
 
     def get_dio(self):
         behavioral_event = BehavioralEvents(name='list of processed DIO`s',)
-        timestamps = {}
-        timeseries = {}
         for i in range(len(self.dio_paths)):
             dio_set = self.dio_paths[i]
             time_set = self.time_paths[i]
-            for dio_time_series in self.metadata['behavioral_events']:
-                timestamps[dio_time_series['name']] = []
-                timeseries[dio_time_series['name']] = []
             continuous_time_file = None
             for file in os.listdir(self.data_path + '/' + time_set):
                 if file.endswith('continuoustime.dat'):
@@ -33,30 +29,32 @@ class DioExtractor:
                 raise MissingDataException("continuous time file not found")
             continuous_time = readTrodesExtractedDataFile(continuous_time_file)
             continuous_time_dict = {str(data[0]): float(data[1]) for data in continuous_time['data']}
-            for dio_time_series in self.metadata['behavioral_events']:
-                temp_timeseries = []
-                temp_timestamps = []
-                for dio_file in os.listdir(self.data_path + '/' + dio_set):
-                    if dio_time_series['name'] + '.' in dio_file:
-                        dio_data = readTrodesExtractedDataFile(self.data_path + '/' + dio_set + '/' + dio_file)
-                        for recorded_event in dio_data['data']:
-                            temp_timeseries.append(recorded_event[1])
-                            key = str(recorded_event[0])
-                            try:
-                                value = continuous_time_dict[key]
-                                temp_timestamps.append(float(value) / 1E9)
-                            except KeyError:
-                                temp_timestamps.append(float('nan'))
-                timestamps[dio_time_series['name']].extend(temp_timestamps)
-                timeseries[dio_time_series['name']].extend(temp_timeseries)
-        for dio_time_series in self.metadata['behavioral_events']:
+            self.save_timeseries(behavioral_event)
+
+    def create_timeseries(self, continuous_time_dict):
+        for dio_time_series in self.all:
+            for dio_file in os.listdir(self.data_path + '/' + dio_set):
+                if dio_time_series['name'] + '.' in dio_file:
+                    dio_data = readTrodesExtractedDataFile(self.data_path + '/' + dio_set + '/' + dio_file)
+                    for recorded_event in dio_data['data']:
+                        single_timeseries = create_timeseries_for_single_event(dio_time_series, recorded_event,
+                                                                               continuous_time_dict)
+
+    def create_timeseries_for_single_event(self, time_series, event, continuous_time_dict):
+        time_series["dio_timeseries"].append(event[1])
+        key = str(event[0])
+        try:
+            value = continuous_time_dict[key]
+            time_series["dio_timestamps"].append(float(value) / 1E9)
+        except KeyError:
+            time_series["dio_timestamps"].append(float('nan'))
+
+    def save_timeseries(self, behavioral_event, all_time_series):
+        for dio_time_series in self.all_time_series:
             behavioral_event.add_timeseries(time_series=TimeSeries(name=dio_time_series['name'],
-                                                                   data=timeseries[dio_time_series['name']],
-                                                                   timestamps=timestamps[dio_time_series['name']],
+                                                                   data=dio_time_series["dio_timeseries"],
+                                                                   timestamps=dio_time_series["dio_timestamps"],
                                                                    description=dio_time_series['description'],
                                                                    )
                                             )
-
-
-        return behavioral_event
 
