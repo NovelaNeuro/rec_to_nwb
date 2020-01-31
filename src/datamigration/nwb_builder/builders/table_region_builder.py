2c52ba73d08db172bb3e55034dd28aa1459ca553
acwikla <acwikla@novelaneuro.com>
2020-01-31 13:46:57 +0100

    + electrode_table_region_builder

--- src/datamigration/nwb_builder/builders/table_region_builder.py
+++ src/datamigration/nwb_builder/builders/table_region_builder.py
@@ -1,15 +1,12 @@
-from hdmf.common import DynamicTableRegion
-
 
 class TableRegionBuilder:
 
     def __init__(self, metadata):
         self.metadata = metadata
 
-    def build(self):
-        region = DynamicTableRegion(
+    def build(self, nwb_content):
+        return nwb_content.create_electrode_table_region(
             description=self.metadata['electrode region']['description'],
             region=self.metadata['electrode region']['region'],
             name='electrodes'
         )
-        return region
