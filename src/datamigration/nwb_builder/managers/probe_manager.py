c66c3de2b598f5348e97447fb3421d95eeedddbe
acwikla <acwikla@novelaneuro.com>
2020-02-04 15:41:07 +0100

    - Probably unused files from old master

--- src/datamigration/nwb_builder/managers/probe_manager.py
+++
@@ -1,8 +0,0 @@
-class ProbeManager:
-
-    @staticmethod
-    def get_probe_file(probes, device_type):
-        for probe_metadata in probes:
-            if probe_metadata['probe_type'] == device_type:
-                return probe_metadata
-        return None
