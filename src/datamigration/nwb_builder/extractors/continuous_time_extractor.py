68bbfdb82614c220b13c966c1d8ba25f74b656e1
MarcinAtNovela <55557514+MarcinAtNovela@users.noreply.github.com>
2020-02-05 13:08:04 +0100

    user/acwikla/ext_versions (#119) (#120)
    * + ext_v param
    
    * + ext_v param
    
    Co-authored-by: acwikla <55695978+acwikla-novela@users.noreply.github.com>

--- src/datamigration/nwb_builder/extractors/continuous_time_extractor.py
+++ src/datamigration/nwb_builder/extractors/continuous_time_extractor.py
@@ -6,5 +6,5 @@ class ContinuousTimeExtractor:
     def get_continuous_time_dict(dataset):
         continuous_time_file = dataset.get_continuous_time()
         continuous_time = readTrodesExtractedDataFile(continuous_time_file)
-        continuous_time_dict = {str(data[0]): float(data[1]) for data in continuous_time['data']}
+        continuous_time_dict = {data[0]: float(data[1]) for data in continuous_time['data']}
         return continuous_time_dict
