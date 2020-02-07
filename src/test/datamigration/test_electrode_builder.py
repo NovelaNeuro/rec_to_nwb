adcab7adb926a59a51057b12e0c569dd609a4597
acwikla <acwikla@novelaneuro.com>
2020-02-07 12:32:34 +0100

    Tests fix

--- src/test/datamigration/test_electrode_builder.py
+++ src/test/datamigration/test_electrode_builder.py
@@ -1,7 +1,10 @@
 import os
+from datetime import datetime
 from unittest import TestCase
 from unittest.mock import Mock
 
+from dateutil.tz import tzlocal
+from hdmf.common import DynamicTable, VectorData, ElementIdentifiers
 from pynwb import NWBFile
 
 from src.datamigration.extension.fl_electrode_group import FLElectrodeGroup
@@ -43,25 +46,79 @@ class TestElectrodeBuilder(TestCase):
 
         self.electrodes_builder = ElectrodeBuilder(probes, metadata)
 
-    def test_build_successful_creation_true(self):
-        # given
-        mock_nwb = Mock()
-        mock_nwb.__class__ = NWBFile
+        self.nwb_file = NWBFile(
+            session_description='demonstrate external files',
+            identifier='NWBE1',
+            session_start_time=datetime(2017, 4, 3, 11, tzinfo=tzlocal()),
+            file_create_date=datetime(2017, 4, 15, 12, tzinfo=tzlocal())
+        )
 
-        mock_eg_1 = Mock()
-        mock_eg_2 = Mock()
-        mock_eg_1.__class__ = FLElectrodeGroup
-        mock_eg_2.__class__ = FLElectrodeGroup
-        electrode_group_object_dict = {0: mock_eg_1, 1: mock_eg_2}
+        self.mock_eg_1 = Mock()
+        self.mock_eg_2 = Mock()
+        self.mock_eg_1.__class__ = FLElectrodeGroup
+        self.mock_eg_2.__class__ = FLElectrodeGroup
+        self.mock_eg_1.name = 'FLElectrodeGroup1'
+        self.mock_eg_2.name = 'FLElectrodeGroup2'
+        electrode_group_object_dict = {0: self.mock_eg_1, 1: self.mock_eg_2}
 
-        # when
         self.electrodes_builder.build(
-            nwb_content=mock_nwb,
+            nwb_content=self.nwb_file,
             electrode_group_dict=electrode_group_object_dict,
         )
 
-        # then
-        # print(mock_nwb.electrodes)
-        # self.assertEqual(256, len(electrodes))
-        # self.assertIsInstance(electrodes[0], DynamicTable)
-        # self.assertIsInstance(electrodes[1], DynamicTable)
+    def test_build_correctObjectLength_true(self):
+
+        print(self.nwb_file.electrodes)
+        self.assertEqual(12, len(self.nwb_file.electrodes))
+
+    def test_build_returnCorrectType_true(self):
+        self.assertIsInstance(self.nwb_file.electrodes, DynamicTable)
+
+        self.assertIsInstance(self.nwb_file.electrodes['x'], VectorData)
+        self.assertIsInstance(self.nwb_file.electrodes['y'], VectorData)
+        self.assertIsInstance(self.nwb_file.electrodes['z'], VectorData)
+        self.assertIsInstance(self.nwb_file.electrodes['imp'], VectorData)
+        self.assertIsInstance(self.nwb_file.electrodes['location'], VectorData)
+        self.assertIsInstance(self.nwb_file.electrodes['filtering'], VectorData)
+        self.assertIsInstance(self.nwb_file.electrodes['group'], VectorData)
+        self.assertIsInstance(self.nwb_file.electrodes['group_name'], VectorData)
+        self.assertIsInstance(self.nwb_file.electrodes.id, ElementIdentifiers)
+        self.assertIsInstance(self.nwb_file.electrodes.description, str)
+
+        self.assertIsInstance(self.nwb_file.electrodes[0][0], int)
+        self.assertIsInstance(self.nwb_file.electrodes[0][1], float)
+        self.assertIsInstance(self.nwb_file.electrodes[0][2], float)
+        self.assertIsInstance(self.nwb_file.electrodes[0][3], float)
+        self.assertIsInstance(self.nwb_file.electrodes[0][4], float)
+        self.assertIsInstance(self.nwb_file.electrodes[0][5], str)
+        self.assertIsInstance(self.nwb_file.electrodes[0][6], str)
+        self.assertIsInstance(self.nwb_file.electrodes[0][7], FLElectrodeGroup)
+        self.assertIsInstance(self.nwb_file.electrodes[0][8], str)
+
+    def test_build_returnCorrectValues_true(self):
+        self.assertEqual(self.nwb_file.electrodes[0][0], 0)
+        self.assertEqual(self.nwb_file.electrodes[1][0], 1)
+
+        self.assertEqual(self.nwb_file.electrodes[0][1], 0.0)
+        self.assertEqual(self.nwb_file.electrodes[1][1], 0.0)
+
+        self.assertEqual(self.nwb_file.electrodes[0][2], 0.0)
+        self.assertEqual(self.nwb_file.electrodes[1][2], 0.0)
+
+        self.assertEqual(self.nwb_file.electrodes[0][3], 0.0)
+        self.assertEqual(self.nwb_file.electrodes[1][3], 0.0)
+
+        self.assertEqual(self.nwb_file.electrodes[0][4], 0.0)
+        self.assertEqual(self.nwb_file.electrodes[1][4], 0.0)
+
+        self.assertEqual(self.nwb_file.electrodes[0][5], 'None')
+        self.assertEqual(self.nwb_file.electrodes[1][5], 'None')
+
+        self.assertEqual(self.nwb_file.electrodes[0][6], 'None')
+        self.assertEqual(self.nwb_file.electrodes[1][6], 'None')
+
+        self.assertEqual(self.nwb_file.electrodes[0][7], self.mock_eg_1)
+        self.assertEqual(self.nwb_file.electrodes[1][7], self.mock_eg_1)
+
+        self.assertEqual(self.nwb_file.electrodes[0][8], 'FLElectrodeGroup1')
+        self.assertEqual(self.nwb_file.electrodes[1][8], 'FLElectrodeGroup1')
