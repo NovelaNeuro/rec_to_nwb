1cb76d1e5c9d140b5637de1d0a1a48ee7822cb86
Wojciech Merynda <wmerynda@novelaneuro.com>
2020-02-07 15:59:46 +0100

    refactor headers processing

--- src/datamigration/nwb_builder/nwb_builder_tools/header_checker/header_comparator.py
+++ src/datamigration/nwb_builder/nwb_builder_tools/header_checker/header_comparator.py
@@ -1,10 +1,14 @@
+from xmldiff import main
+
+
 class HeaderComparator:
 
     def __init__(self, xml_headers):
         self.xml_headers = xml_headers
 
     def compare(self):
-        if len(self.xml_headers) > 1:
-            header_1 = self.xml_headers[0]
-            return all(header == header_1 for header in self.xml_headers)
-        return True
+        headers_differences = []
+        first_xml_file = self.xml_headers[0]
+        for xml_file in self.xml_headers:
+            headers_differences += main.diff_files(first_xml_file, xml_file)
+        return headers_differences
