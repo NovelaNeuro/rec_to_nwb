586f6b1b91767e964820b4ec73b2ddb6cd0a692c
acwikla <55695978+acwikla-novela@users.noreply.github.com>
2020-02-11 14:43:23 +0100

    user/acwikla/move_mda_object (#132)

--- src/datamigration/nwb_builder/extractors/mda_extractor.py
+++ src/datamigration/nwb_builder/extractors/mda_extractor.py
@@ -2,11 +2,13 @@ from src.datamigration.exceptions.missing_data_exception import MissingDataExcep
 from src.datamigration.nwb_builder.managers.mda_data_manager import MdaDataManager
 from src.datamigration.nwb_builder.managers.mda_timestamp_data_manager import MdaTimestampDataManager
 from src.datamigration.nwb_builder.nwb_builder_tools.data_iterator import DataIterator, DataIterator1D
-from src.datamigration.nwb_builder.nwb_builder_tools.mda_object import MdaObject
+from src.datamigration.nwb_builder.others.mda_content import MdaContent
 
 
 # ToDo Is it SOLID?
-# Should be: extract here and manager to get MdaObject
+# Should be: extract here and manager to get MdaContent
+
+
 class MdaExtractor:
 
     def __init__(self, datasets):
@@ -34,4 +36,4 @@ class MdaExtractor:
         timestamps = MdaTimestampDataManager(directories=[self.timestamps], continuous_time_directories=self.continuous_time)
         extracted_timestamps = DataIterator1D(timestamps)
 
-        return MdaObject(extracted_mda, extracted_timestamps)
+        return MdaContent(extracted_mda, extracted_timestamps)
