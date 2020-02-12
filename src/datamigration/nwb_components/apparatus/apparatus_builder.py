7b9d90ca1e559e85054a0503e1735af6c6a32468
Wojciech Merynda <wmerynda@novelaneuro.com>
2020-02-12 13:11:59 +0100

    proper static method call

--- src/datamigration/nwb_components/apparatus/apparatus_builder.py
+++ src/datamigration/nwb_components/apparatus/apparatus_builder.py
@@ -6,8 +6,7 @@ class ApparatusBuilder:
 
     def __init__(self, apparatus_metadata):
         self.apparatus_extractor = ApparatusExtractor(apparatus_metadata)
-        self.apparatus_creator = ApparatusCreator()
 
     def build(self):
         edges, nodes = self.apparatus_extractor.get_data()
-        return self.apparatus_creator.create_apparatus(edges, nodes)
+        return ApparatusCreator.create_apparatus(edges, nodes)
