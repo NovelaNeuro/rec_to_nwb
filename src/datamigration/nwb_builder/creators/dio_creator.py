450fc09c8cf12b504af4436a4f2f7e93751f193d
wbodo <55714242+wbodo@users.noreply.github.com>
2020-02-16 23:57:02 +0100

    adding dio_builder class with multithreading

--- src/datamigration/nwb_builder/creators/dio_creator.py
+++ src/datamigration/nwb_builder/creators/dio_creator.py
@@ -3,7 +3,7 @@ from pynwb import TimeSeries
 
 class DioCreator:
     @classmethod
-    def create_dio_time_series(cls,behavioral_event, all_dio_time_series):
+    def create_dio_time_series(cls, behavioral_event, all_dio_time_series):
         for dio_time_series in all_dio_time_series:
             behavioral_event.add_timeseries(time_series=TimeSeries(name=dio_time_series['name'],
                                                                    data=dio_time_series["dio_timeseries"],
