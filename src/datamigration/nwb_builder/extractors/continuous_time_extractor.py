9cc609ba10af89875e1a6bba4ad41f8931e4e2de
Wojciech Merynda <48327847+WojciechMerynda@users.noreply.github.com>
2020-02-19 13:44:22 +0100

    User/mbukowski/dio part 1 (#158)
    * cleanup
    
    * refactoring
    
    * extractor and its tests
    
    * extractor and its tests
    
    * sratch of changes
    
    * injector test
    
    * merger for dio data
    
    * filter dio files
    
    * adding dio_builder class with multithreading
    
    * add new function to dio extractor
    
    * fix
    
    * fix
    
    * dio extractor new extract_dio
    
    * filter dio files refactor
    
    * filter dio files syntatic sugar
    
    * filter dio files syntatic sugar style clean
    
    * filter dio files syntatic sugar style clean
    
    * update dio extractor
    
    * update dio extractor
    
    * merge dio data change
    
    * extractor changes
    
    * recover dio_extractor
    
    * dio builder
    
    * to dos
    
    * update builder
    
    * dio refactor finnish need fix tests
    
    * test refactor
    
    * fix dio extractor and manager
    
    * fix not working dio
    
    * fix not working dio
    
    * move to proper directory
    
    * change skiptest
    
    Co-authored-by: MarcinAtNovela <55557514+MarcinAtNovela@users.noreply.github.com>
    Co-authored-by: wbodo <55714242+wbodo@users.noreply.github.com>

--- src/datamigration/nwb_builder/extractors/continuous_time_extractor.py
+++ src/datamigration/nwb_builder/extractors/continuous_time_extractor.py
@@ -2,13 +2,13 @@ from rec_to_binaries.read_binaries import readTrodesExtractedDataFile
 
 
 class ContinuousTimeExtractor:
+
     @staticmethod
-    def get_continuous_time_dict_dataset(dataset):
-        continuous_time_file = dataset.get_continuous_time()
-        continuous_time = readTrodesExtractedDataFile(continuous_time_file)
-        return {str(data[0]): float(data[1]) for data in continuous_time['data']}
+    def get_continuous_time_dict(files):
+        return [ContinuousTimeExtractor.get_continuous_time_dict_file(file) for file in files]
 
     @staticmethod
     def get_continuous_time_dict_file(file):
         continuous_time = readTrodesExtractedDataFile(file)
         return {str(data[0]): float(data[1]) for data in continuous_time['data']}
+
--- src/datamigration/nwb_builder/nwb_builder_tools/timestamp_converter.py
+++ src/datamigration/nwb_builder/nwb_builder_tools/timestamp_converter.py
@@ -10,16 +10,15 @@ logger = logging.getLogger(__name__)
 
 
 class TimestampConverter:
+
     @staticmethod
     def convert_timestamps(continuous_time_dict, timestamps):
         converted_timestamps = np.ndarray([np.shape(timestamps)[0], ], dtype="float64")
         for i in range(np.shape(timestamps)[0]):
             key = str(timestamps[i])
-            try:
-                value = continuous_time_dict[key]
-                converted_timestamps[i] = float(value) / 1E9
-            except KeyError as error:
-                message = 'Following key: ' + str(key) + ' does not exist!' + str(error)
+            value = continuous_time_dict.get(key, float('nan')) / 1E9
+            if np.isnan(value):
+                message = 'Following key: ' + str(key) + ' does not exist in continioustime dictionary!'
                 logger.exception(message)
-                converted_timestamps[i] = float('nan')
+            converted_timestamps[i] = value
         return converted_timestamps
