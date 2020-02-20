37dc837988fd4f76d353255680104f27ba934845
acwikla <acwikla@novelaneuro.com>
2020-02-20 15:30:58 +0100

    Merge remote-tracking branch 'origin/master' into user/wmerynda/refactor_test_dio_manager
    # Conflicts:
    #	src/datamigration/nwb/components/dio/dio_extractor.py
    #	src/datamigration/nwb/components/dio/dio_files.py
    #	src/datamigration/nwb/components/dio/dio_manager.py
    #	src/datamigration/nwb_file_builder.py
    #	src/test/e2etests/test_nwb_full_generation.py

--- src/test/datamigration/dio/test_dio_manager.py
+++ src/test/datamigration/dio/test_dio_manager.py
@@ -36,12 +36,12 @@ class TestDioManager(unittest.TestCase):
             {'name': 'Din2', 'description': 'Poke2'}]
 
         dio_files = [{
-            'Din1': path + '/../test_data/beans/preprocessing/20190718/20190718_beans_01_s1.DIO//20190718_beans_01_s1.dio_Din1.dat',
-            'Din2': path + '/../test_data/beans/preprocessing/20190718/20190718_beans_01_s1.DIO//20190718_beans_01_s1.dio_Din2.dat',
+            'Din1': path + '/../../test_data/beans/preprocessing/20190718/20190718_beans_01_s1.DIO//20190718_beans_01_s1.dio_Din1.dat',
+            'Din2': path + '/../../test_data/beans/preprocessing/20190718/20190718_beans_01_s1.DIO//20190718_beans_01_s1.dio_Din2.dat',
         },
             {
-                'Din1': path + '/../test_data/beans/preprocessing/20190718/20190718_beans_01_s1.DIO//20190718_beans_01_s1.dio_Din1.dat',
-                'Din2': path + '/../test_data/beans/preprocessing/20190718/20190718_beans_01_s1.DIO//20190718_beans_01_s1.dio_Din2.dat',
+                'Din1': path + '/../../test_data/beans/preprocessing/20190718/20190718_beans_01_s1.DIO//20190718_beans_01_s1.dio_Din1.dat',
+                'Din2': path + '/../../test_data/beans/preprocessing/20190718/20190718_beans_01_s1.DIO//20190718_beans_01_s1.dio_Din2.dat',
             },
         ]
 
@@ -60,8 +60,7 @@ class TestDioManager(unittest.TestCase):
         cls.din_2_array = np.array(1367266)
         cls.din_2_list = [0]
 
-        cls.dio_manager.dio_extractor = Mock()
-        cls.dio_manager.dio_extractor.__class__ = DioExtractor
+        cls.dio_manager.dio_extractor = Mock(spec=DioExtractor)
         cls.dio_manager.dio_extractor.extract_dio_for_single_dataset.return_value = {
             'Din1': [cls.din_1_array, cls.din_1_list],
             'Din2': [cls.din_2_array, cls.din_2_list],
