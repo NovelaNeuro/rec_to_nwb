7171b62cd6685228ff59567e42b23b60fcd78e9d
acwikla <acwikla@novelaneuro.com>
2020-02-20 16:02:00 +0100

    test_dio_extractor

--- src/datamigration/nwb_builder/nwb_builder_tools/timestamp_converter.py
+++ src/datamigration/nwb_builder/nwb_builder_tools/timestamp_converter.py
@@ -13,9 +13,7 @@ class TimestampConverter:
 
     @staticmethod
     def convert_timestamps(continuous_time_dict, timestamps):
-        # ToDO shape = len(timestamp)
         converted_timestamps = np.ndarray(shape=[np.shape(timestamps)[0], ], dtype="float64")
-        # ToDo for counter, timestamp in enumerate(timestamps):
         for i in range(np.shape(timestamps)[0]):
             key = str(timestamps[i])
             value = continuous_time_dict.get(key, float('nan')) / 1E9
--- src/test/datamigration/dio/test_dio_extractor.py
+++ src/test/datamigration/dio/test_dio_extractor.py
@@ -1,9 +1,9 @@
 import os
 from unittest import TestCase
-from unittest.mock import Mock
+
+from pandas import np, array
 
 from src.datamigration.nwb.components.dio.dio_extractor import DioExtractor
-from src.datamigration.nwb_builder.nwb_builder_tools.timestamp_converter import TimestampConverter
 
 path = os.path.dirname(os.path.abspath(__file__))
 
@@ -29,132 +29,59 @@ class TestDioExtractor(TestCase):
         )
 
     def test_extracted_dio_for_single_dataset_correctType_true(self):
-        print(self.single_dataset_data)
+        self.assertIsInstance(self.single_dataset_data, dict)
+        self.assertIsInstance(self.single_dataset_data['Din1'], list)
+        self.assertIsInstance(self.single_dataset_data['Din1'][0], np.ndarray)
+        self.assertIsInstance(self.single_dataset_data['Din1'][1], list)
 
-# import contextlib
-# import os
-# import unittest
-# from pathlib import Path
-# from unittest import mock
-# from unittest.mock import Mock
-#
-# import pytest
-# from pandas import np, array
-#
-# from src.datamigration.nwb.components.dio.dio_extractor import DioExtractor
-# from src.datamigration.nwb.components.dio.dio_files import DioFiles
-# from src.datamigration.nwb_builder.extractors.continuous_time_extractor import ContinuousTimeExtractor
-# from src.datamigration.nwb.components.dio.dio_manager import DioManager
-# from src.datamigration.nwb_builder.managers.metadata_manager import MetadataManager
-#
-# path = os.path.dirname(os.path.abspath(__file__))
-#
-#
-# class TestDioManager(unittest.TestCase):
-#
-#     @classmethod
-#     def setUpClass(cls):
-#         continuous_time_dicts = [
-#             {1367266: 19191919, 9599570: 74747474, 9603169: 89898989, 9610303: 54545454, 9619154: 32323232,
-#              9612481: 67676767, 9619802: 50505050, 9627552: 41414141, 9643239: 23232323, 9644490: 21212121,
-#              9645544: 40404040, 9645721: 66666666, 9646074: 48484848, 9644629: 31313131, 9641056: 55555555},
-#
-#             {1367266: 19191919, 9599570: 74747474, 9603169: 89898989, 9610303: 54545454, 9619154: 32323232,
-#              9612481: 67676767, 9619802: 50505050, 9627552: 41414141, 9643239: 23232323, 9644490: 21212121,
-#              9645544: 40404040, 9645721: 66666666, 9646074: 48484848, 9644629: 31313131, 9641056: 55555555}
-#         ]
-#
-#         dio_metadata = [
-#             {'name': 'Din1', 'description': 'Poke1'},
-#             {'name': 'Din2', 'description': 'Poke2'}]
-#
-#         dio_files = [{
-#             'Din1': path + '/../test_data/beans/preprocessing/20190718/20190718_beans_01_s1.DIO//20190718_beans_01_s1.dio_Din1.dat',
-#             'Din2': path + '/../test_data/beans/preprocessing/20190718/20190718_beans_01_s1.DIO//20190718_beans_01_s1.dio_Din2.dat',
-#         },
-#             {
-#                 'Din1': path + '/../test_data/beans/preprocessing/20190718/20190718_beans_01_s1.DIO//20190718_beans_01_s1.dio_Din1.dat',
-#                 'Din2': path + '/../test_data/beans/preprocessing/20190718/20190718_beans_01_s1.DIO//20190718_beans_01_s1.dio_Din2.dat',
-#             },
-#         ]
-#
-#         cls.dio_manager = DioManager(
-#             dio_files=dio_files,
-#             dio_metadata=dio_metadata,
-#             continuous_time_dicts=continuous_time_dicts)
-#
-#         cls.din_1_array = np.array(
-#             [1367266, 9599570, 9603169, 9610303, 9612481,
-#              9619154, 9619802, 9627552, 9641056, 9643239,
-#              9644490, 9644629, 9645544, 9645721, 9646074]
-#         )
-#         cls.din_1_list = [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0]
-#
-#         cls.din_2_array = np.array(1367266)
-#         cls.din_2_list = [0]
-#
-#         cls.dio_manager.dio_extractor = Mock()
-#         cls.dio_manager.dio_extractor.__class__ = DioExtractor
-#         cls.dio_manager.dio_extractor.extract_dio_for_single_dataset.return_value = {
-#             'Din1': [cls.din_1_array, cls.din_1_list],
-#             'Din2': [cls.din_2_array, cls.din_2_list],
-#         }
-#         cls.dio = cls.dio_manager.get_dio()
-#
-#     def test_get_dio_returnCorrectType_true(self):
-#         self.assertIsInstance(self.dio, dict)
-#         self.assertIsInstance(self.dio['Din1'][0], np.ndarray)
-#         self.assertIsInstance(self.dio['Din1'][1], list)
-#         self.assertIsInstance(self.dio['Din2'][0], np.ndarray)
-#         self.assertIsInstance(self.dio['Din2'][1], list)
-#
-#     def test_get_dio_returnCorrectValue_true(self):
-#         self.assertEqual(
-#             self.dio,
-#             {
-#                 'Din1': [
-#                     array([
-#                         1367266, 9599570, 9603169, 9610303, 9612481, 9619154, 9619802,
-#                         9627552, 9641056, 9643239, 9644490, 9644629, 9645544, 9645721,
-#                         9646074, 1367266, 9599570, 9603169, 9610303, 9612481, 9619154,
-#                         9619802, 9627552, 9641056, 9643239, 9644490, 9644629, 9645544,
-#                         9645721, 9646074
-#                     ]),
-#                     [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0]],
-#                 'Din2': [
-#                     array([1367266, 1367266]),
-#                     [0, 0]]}
-#         )
-#         self.assertEqual(
-#             self.dio['Din1'][0],
-#             array([
-#                 1367266, 9599570, 9603169, 9610303, 9612481, 9619154, 9619802,
-#                 9627552, 9641056, 9643239, 9644490, 9644629, 9645544, 9645721,
-#                 9646074, 1367266, 9599570, 9603169, 9610303, 9612481, 9619154,
-#                 9619802, 9627552, 9641056, 9643239, 9644490, 9644629, 9645544,
-#                 9645721, 9646074
-#             ])
-#         )
-#         self.assertEqual(
-#             self.dio['Din1'][1],
-#             [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0]
-#         )
-#         self.assertEqual(
-#             self.dio['Din2'][0],
-#             array([1367266, 1367266])
-#         )
-#         self.assertEqual(
-#             self.dio['Din2'][1],
-#             [0, 0]
-#         )
-#
-#     def test_get_dio_returnCorrectShape_true(self):
-#         self.assertEqual(len(self.dio), 2)
-#
-#         self.assertEqual(len(self.dio['Din1']), 2)
-#         self.assertEqual(self.dio['Din1'][0].shape, (30,))
-#         self.assertEqual(len(self.dio['Din1'][1]), 30)
-#
-#         self.assertEqual(len(self.dio['Din2']), 2)
-#         self.assertEqual(self.dio['Din2'][0].shape, (2,))
-#         self.assertEqual(len(self.dio['Din2'][0]), 2)
+        self.assertIsInstance(self.single_dataset_data['Din2'], list)
+        self.assertIsInstance(self.single_dataset_data['Din2'][0], np.ndarray)
+        self.assertIsInstance(self.single_dataset_data['Din2'][1], list)
+
+    def test_extracted_dio_for_single_dataset_correctValues_true(self):
+        print(self.single_dataset_data)
+        self.assertEqual(
+            self.single_dataset_data,
+            {
+                'Din1': [
+                    array([
+                        0.01919192, 0.07474747, 0.08989899, 0.05454545, 0.06767677,
+                        0.03232323, 0.05050505, 0.04141414, 0.05555555, 0.02323232,
+                        0.02121212, 0.03131313, 0.04040404, 0.06666667, 0.04848485
+                    ]),
+                    [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0]],
+                'Din2': [
+                    array([0.01919192]),
+                    [0]]}
+        )
+        self.assertEqual(
+            self.single_dataset_data['Din1'][0],
+            array([
+                0.01919192, 0.07474747, 0.08989899, 0.05454545, 0.06767677,
+                0.03232323, 0.05050505, 0.04141414, 0.05555555, 0.02323232,
+                0.02121212, 0.03131313, 0.04040404, 0.06666667, 0.04848485
+            ])
+        )
+        self.assertEqual(
+            self.single_dataset_data['Din1'][1],
+            [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0]
+        )
+        self.assertEqual(
+            self.single_dataset_data['Din2'][0],
+            array([0.01919192])
+        )
+        self.assertEqual(
+            self.single_dataset_data['Din2'][1],
+            [0]
+        )
+
+    def test_extracted_dio_for_single_dataset_returnCorrectShape_true(self):
+        self.assertEqual(len(self.single_dataset_data), 2)
+
+        self.assertEqual(len(self.single_dataset_data['Din1']), 2)
+        self.assertEqual(self.single_dataset_data['Din1'][0].shape, (15,))
+        self.assertEqual(len(self.single_dataset_data['Din1'][1]), 15)
+
+        self.assertEqual(len(self.single_dataset_data['Din2']), 2)
+        self.assertEqual(self.single_dataset_data['Din2'][0].shape, (1,))
+        self.assertEqual(len(self.single_dataset_data['Din2'][1]), 1)
