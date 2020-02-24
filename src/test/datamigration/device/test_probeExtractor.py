7df3df8ee4da4e6e1b16cf1a2034498600ebfbae
acwikla <acwikla@novelaneuro.com>
2020-02-24 13:53:53 +0100

    testing temp file and temp dir

--- src/test/datamigration/device/test_probeExtractor.py
+++ src/test/datamigration/device/test_probeExtractor.py
@@ -26,26 +26,27 @@ class TestProbeExtractor(TestCase):
         # )
 
     def test_LoadOperation(self):
-        contents = "---\nfoo: blargh\nblee:\n - blah\n - blorp\n"
+        contents = '---\nfoo: something\nfoo2:\n - first\n - second\n'
 
         with tempfile.TemporaryDirectory(dir=path) as dirpath:
-            testfile = os.path.join(dirpath, 'file1')
+            testfile = os.path.join(dirpath, 'file1.yml')
             with open(testfile, 'w') as outfile:
                 outfile.write(contents)
 
-            with mock.patch('yaml.safe_load') as safe_load:
-                def loader(f):
-                    safe_load.file_contents = f.read()
-                    return safe_load.return_value
+                with mock.patch('yaml.safe_load') as safe_load:
+                    def loader(f):
+                        safe_load.file_contents = f.read()
+                        return safe_load.return_value
 
 
-                safe_load.side_effect = loader
-                result = self.probes_extractor.extract_probes_metadata(testfile)
+                    safe_load.side_effect = loader
+                    # result = self.probes_extractor.extract_probes_metadata([outfile])
+                    result = self.probes_extractor.extract_probes_metadata([dirpath + '/file1.yml'])
 
-                # yaml.safe_load called once
-                self.assertEqual(1, len(safe_load.call_args_list))
-                self.assertEqual(contents, safe_load.file_contents)
-                self.assertEqual(result, safe_load.return_value)
+                    # yaml.safe_load called once
+                    self.assertEqual(1, len(safe_load.call_args_list))
+                    self.assertEqual(contents, safe_load.file_contents)
+                    self.assertEqual(result, safe_load.return_value)
 
     # def test_extractProbesMetadata_successful_true(self):
     #     self.assertIsNotNone(self.probes_content)
