031d214677937ab94d26639454b668f82eab99a2
acwikla <55695978+acwikla-novela@users.noreply.github.com>
2020-02-24 21:04:20 +0100

    User/acwikla/info logging (#174)

--- src/datamigration/nwb_builder/creators/processing_module_creator.py
+++ src/datamigration/nwb_builder/creators/processing_module_creator.py
@@ -1,4 +1,12 @@
+import logging.config
+import os
+
 from pynwb import ProcessingModule
+
+path = os.path.dirname(os.path.abspath(__file__))
+
+logging.config.fileConfig(fname=str(path) + '/../../../logging.conf', disable_existing_loggers=False)
+logger = logging.getLogger(__name__)
 
 
 class ProcessingModuleCreator:
@@ -11,4 +19,4 @@ class ProcessingModuleCreator:
             self.processing_module.add_data_interface(data)
         except TypeError as err:
             # log error instead
-            print(err)
+            logger.error('Inserting data into processing module has failed: ' + str(err))
