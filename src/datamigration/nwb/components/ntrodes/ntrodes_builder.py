c5109ea2fb97ea238f0d3c28ca3ce7c33b232b34
acwikla <acwikla@novelaneuro.com>
2020-02-25 17:25:50 +0100

    Ntrodes tests LfNTrodes

--- src/datamigration/nwb/components/ntrodes/ntrodes_builder.py
+++ src/datamigration/nwb/components/ntrodes/ntrodes_builder.py
@@ -1,3 +1,4 @@
+from src.datamigration.nwb.components.ntrodes.lf_ntrodes import LfNTrodes
 from src.datamigration.nwb.components.ntrodes.ntrodes_creator import NTrodesCreator
 from src.datamigration.nwb.components.ntrodes.ntrodes_extractor import NTrodesExtractor
 from src.datamigration.nwb.components.ntrodes.ntrodes_injector import NTrodesInjector
@@ -9,13 +10,10 @@ class NTrodesBuilder:
         self.metadata = metadata
 
         self.ntrodes_extractor = NTrodesExtractor()
-        self.ntrodes_creator = NTrodesCreator()
-        self.ntrodes_injector = NTrodesInjector()
 
     def build(self, nwb_content):
-        for ntrode_metadata in self.metadata['ntrode probe channel map']:
-            device = self.ntrodes_extractor.extract_device(ntrode_metadata, nwb_content)
-            map_list = self.ntrodes_extractor.extract_map(ntrode_metadata)
-
-            ntrode = self.ntrodes_creator.create_ntrode(ntrode_metadata, device, map_list)
-            self.ntrodes_injector.inject_ntrode(nwb_content, ntrode)
+        return [LfNTrodes(
+            metadata=ntrode_metadata,
+            device=self.ntrodes_extractor.extract_device(ntrode_metadata, nwb_content),
+            map_list=self.ntrodes_extractor.extract_map(ntrode_metadata)
+        ) for ntrode_metadata in self.metadata['ntrode probe channel map']]
