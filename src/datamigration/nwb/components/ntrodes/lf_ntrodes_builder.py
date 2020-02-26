2fa699e949aaea58305c1b265af1ad79d6833a00
acwikla <acwikla@novelaneuro.com>
2020-02-26 11:48:35 +0100

    new name convention

--- src/datamigration/nwb/components/ntrodes/lf_ntrodes_builder.py
+++ src/datamigration/nwb/components/ntrodes/lf_ntrodes_builder.py
@@ -1,6 +1,6 @@
 from src.datamigration.nwb.components.ntrodes.lf_ntrodes import LfNTrodes
 
-from src.datamigration.nwb.components.ntrodes.ntrodes_extractor import NTrodesExtractor
+from src.datamigration.nwb.components.ntrodes.lf_ntrodes_extractor import LfNTrodesExtractor
 
 
 class LfNTrodesBuilder:
@@ -8,11 +8,11 @@ class LfNTrodesBuilder:
     def __init__(self, metadata):
         self.metadata = metadata
 
-        self.ntrodes_extractor = NTrodesExtractor()
+        self.lf_ntrodes_extractor = LfNTrodesExtractor()
 
     def build(self, nwb_content):
         return [LfNTrodes(
             metadata=ntrode_metadata,
-            device=self.ntrodes_extractor.extract_device(ntrode_metadata, nwb_content),
-            map_list=self.ntrodes_extractor.extract_map(ntrode_metadata)
+            device=self.lf_ntrodes_extractor.extract_device(ntrode_metadata, nwb_content),
+            map_list=self.lf_ntrodes_extractor.extract_map(ntrode_metadata)
         ) for ntrode_metadata in self.metadata['ntrode probe channel map']]
