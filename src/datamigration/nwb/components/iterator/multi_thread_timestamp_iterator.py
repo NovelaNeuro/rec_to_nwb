e94cbb7665e7e50adf952723863b6ee6370119f8
wbodo <55714242+wbodo@users.noreply.github.com>
2020-02-27 13:39:44 +0100

    adding abstract class for timestamp iterators

--- src/datamigration/nwb/components/iterator/multi_thread_timestamp_iterator.py
+++ src/datamigration/nwb/components/iterator/multi_thread_timestamp_iterator.py
@@ -1,31 +1,22 @@
 import concurrent.futures
 
 import numpy as np
-from hdmf.data_utils import AbstractDataChunkIterator, DataChunk
+from hdmf.data_utils import DataChunk
+
+from src.datamigration.nwb.components.iterator.timestamp_iterator import TimestampIterator
 
 
-class MultiThreadTimestampIterator(AbstractDataChunkIterator):
+class MultiThreadTimestampIterator(TimestampIterator):
 
     def __init__(self, data, number_of_threads=6):
-        self.data = data
+        TimestampIterator.__init__(self, data)
         self.number_of_threads = number_of_threads
-        self.__current_index = 0
-        self.current_dataset = 0
-
-        self.dataset_file_lenght = data.get_file_lenghts_in_datasets()
-        self.number_of_steps = self.data.get_number_of_datasets()
-        self.dataset_file_length = self.data.get_file_lenghts_in_datasets()
-        self.shape = self.data.get_final_data_shape()
-
-    # Override
-    def __iter__(self):
-        return self
 
     # Override
     def __next__(self):
-        if self.__current_index < self.number_of_steps:
+        if self._current_index < self.number_of_steps:
             number_of_threads_in_current_step = min(self.number_of_threads,
-                                                    self.number_of_steps - self.__current_index)
+                                                    self.number_of_steps - self._current_index)
 
             with concurrent.futures.ThreadPoolExecutor() as executor:
                 threads = [executor.submit(MultiThreadTimestampIterator.get_data_from_file,
@@ -38,7 +29,7 @@ class MultiThreadTimestampIterator(AbstractDataChunkIterator):
             selection = self.__get_selection(number_of_threads_in_current_step)
             data_chunk = DataChunk(data=stacked_data_from_multiple_files, selection=selection)
 
-            self.__current_index += number_of_threads_in_current_step
+            self._current_index += number_of_threads_in_current_step
             self.current_dataset += number_of_threads_in_current_step
 
             del stacked_data_from_multiple_files
@@ -48,28 +39,6 @@ class MultiThreadTimestampIterator(AbstractDataChunkIterator):
 
     next = __next__
 
-    @staticmethod
-    def get_data_from_file(data, current_dataset):
-        return data.retrieve_real_timestamps(current_dataset)
-
     def __get_selection(self, number_of_threads_in_current_step):
         return np.s_[sum(self.dataset_file_lenght[0:self.current_dataset]):
                      sum(self.dataset_file_lenght[0:self.current_dataset + number_of_threads_in_current_step]), ]
-
-    # Override
-    def recommended_chunk_shape(self):
-        return None
-
-    # Override
-    def recommended_data_shape(self):
-        return self.shape
-
-    # Override
-    @property
-    def dtype(self):
-        return np.dtype('float64')
-
-    # Override
-    @property
-    def maxshape(self):
-        return self.shape
--- src/datamigration/nwb/components/iterator/single_thread_timestamp_iterator.py
+++ src/datamigration/nwb/components/iterator/single_thread_timestamp_iterator.py
@@ -1,31 +1,18 @@
 import numpy as np
-from hdmf.data_utils import AbstractDataChunkIterator, DataChunk
+from hdmf.data_utils import DataChunk
+
+from src.datamigration.nwb.components.iterator.timestamp_iterator import TimestampIterator
 
 
-class SingleThreadTimestampIterator(AbstractDataChunkIterator):
-
-    def __init__(self, data):
-        self.data = data
-        self.__current_index = 0
-        self.current_dataset = 0
-
-        self.dataset_file_lenght = data.get_file_lenghts_in_datasets()
-        self.number_of_steps = self.data.get_number_of_datasets()
-        self.dataset_file_length = self.data.get_file_lenghts_in_datasets()
-        self.shape = self.data.get_final_data_shape()
-
-    # Override
-    def __iter__(self):
-        return self
-
+class SingleThreadTimestampIterator(TimestampIterator):
     # Override
     def __next__(self):
-        if self.__current_index < self.number_of_steps:
+        if self._current_index < self.number_of_steps:
             data_from_file = self.__get_timestamps()
-            selection = self.__get_selection()
+            selection = self._get_selection()
             data_chunk = DataChunk(data=data_from_file, selection=selection)
 
-            self.__current_index += 1
+            self._current_index += 1
             self.current_dataset += 1
 
             del data_from_file
@@ -41,21 +28,3 @@ class SingleThreadTimestampIterator(AbstractDataChunkIterator):
     def __get_selection(self):
         return np.s_[sum(self.dataset_file_lenght[0:self.current_dataset]):
                      sum(self.dataset_file_lenght[0:self.current_dataset + 1]), ]
-
-    # Override
-    def recommended_chunk_shape(self):
-        return None
-
-    # Override
-    def recommended_data_shape(self):
-        return self.shape
-
-    # Override
-    @property
-    def dtype(self):
-        return np.dtype('float64')
-
-    # Override
-    @property
-    def maxshape(self):
-        return self.shape
