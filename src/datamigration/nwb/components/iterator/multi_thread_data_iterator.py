d33a428a51741746f7cc71570601d27d8dd57622
wbodo <55714242+wbodo@users.noreply.github.com>
2020-02-27 14:10:33 +0100

    add data iterator interface

--- src/datamigration/nwb/components/iterator/multi_thread_data_iterator.py
+++ src/datamigration/nwb/components/iterator/multi_thread_data_iterator.py
@@ -1,32 +1,18 @@
 import concurrent.futures
 
 import numpy as np
-from hdmf.data_utils import AbstractDataChunkIterator, DataChunk
+from hdmf.data_utils import DataChunk
+
+from src.datamigration.nwb.components.iterator.data_iterator import DataIterator
 
 
-class MultiThreadDataIterator(AbstractDataChunkIterator):
-
+class MultiThreadDataIterator(DataIterator):
     def __init__(self, data, number_of_threads=6):
-        self.data = data
+        DataIterator.__init__(self, data)
         self.number_of_threads = number_of_threads
 
-        self.__current_index = 0
-        self.current_file = 0
-        self.current_dataset = 0
-
-        self.number_of_steps = self.data.get_number_of_datasets() * self.data.get_number_of_files_per_dataset()
-        self.dataset_file_length = self.data.get_file_lenghts_in_datasets()
-        self.number_of_rows = self.data.get_number_of_rows_per_file()
-        self.number_of_files_in_single_dataset = self.data.get_number_of_files_per_dataset()
-        self.shape = [self.data.get_final_data_shape()[1], self.data.get_final_data_shape()[0]]
-
-    # Override
-    def __iter__(self):
-        return self
-
-    # Override
     def __next__(self):
-        if self.__current_index < self.number_of_steps:
+        if self._current_index < self.number_of_steps:
             number_of_threads_in_current_step = min(self.number_of_threads,
                                                     self.number_of_files_in_single_dataset - self.current_file)
             with concurrent.futures.ThreadPoolExecutor() as executor:
@@ -44,7 +30,7 @@ class MultiThreadDataIterator(AbstractDataChunkIterator):
                                            number_of_rows=self.number_of_rows)
             data_chunk = DataChunk(data=stacked_data_from_multiple_files, selection=selection)
 
-            self.__current_index += number_of_threads_in_current_step
+            self._current_index += number_of_threads_in_current_step
             self.current_file += number_of_threads_in_current_step
 
             if self.current_file >= self.number_of_files_in_single_dataset:
@@ -62,27 +48,3 @@ class MultiThreadDataIterator(AbstractDataChunkIterator):
     def get_data_from_file(data, current_dataset, current_file):
         return np.transpose(data.read_data(current_dataset, current_file))
 
-    @staticmethod
-    def get_selection(number_of_threads, current_dataset, dataset_file_length, current_file, number_of_rows):
-        return np.s_[sum(dataset_file_length[0:current_dataset]):
-                     sum(dataset_file_length[0:current_dataset + 1]),
-               (current_file * number_of_rows):
-               ((current_file + number_of_threads) * number_of_rows)]
-
-    # Override
-    def recommended_chunk_shape(self):
-        return None
-
-    # Override
-    def recommended_data_shape(self):
-        return self.shape
-
-    # Override
-    @property
-    def dtype(self):
-        return np.dtype('int16')
-
-    # Override
-    @property
-    def maxshape(self):
-        return self.shape
--- src/datamigration/nwb/components/iterator/single_thread_data_iterator.py
+++ src/datamigration/nwb/components/iterator/single_thread_data_iterator.py
@@ -1,34 +1,24 @@
 import numpy as np
-from hdmf.data_utils import AbstractDataChunkIterator, DataChunk
+from hdmf.data_utils import DataChunk
+
+from src.datamigration.nwb.components.iterator.data_iterator import DataIterator
 
 
-class SingleThreadDataIterator(AbstractDataChunkIterator):
+class SingleThreadDataIterator(DataIterator):
 
     def __init__(self, data):
-        self.data = data
+        DataIterator.__init__(self, data)
 
-        self.__current_index = 0
-        self.current_file = 0
-        self.current_dataset = 0
-
-        self.number_of_steps = self.data.get_number_of_datasets() * self.data.get_number_of_files_per_dataset()
-        self.dataset_file_length = self.data.get_file_lenghts_in_datasets()
-        self.number_of_rows = self.data.get_number_of_rows_per_file()
-        self.number_of_files_in_single_dataset = self.data.get_number_of_files_per_dataset()
-        self.shape = [self.data.get_final_data_shape()[1], self.data.get_final_data_shape()[0]]
-
-    # Override
     def __iter__(self):
         return self
 
-    # Override
     def __next__(self):
-        if self.__current_index < self.number_of_steps:
+        if self._current_index < self.number_of_steps:
             data_from_file = self.__get_data_from_file()
-            selection = self.__get_selection()
+            selection = self._get_selection()
             data_chunk = DataChunk(data=data_from_file, selection=selection)
 
-            self.__current_index += 1
+            self._current_index += 1
             self.current_file += 1
 
             if self.current_file >= self.number_of_files_in_single_dataset:
@@ -44,27 +34,3 @@ class SingleThreadDataIterator(AbstractDataChunkIterator):
 
     def __get_data_from_file(self):
         return np.transpose(self.data.read_data(self.current_dataset, self.current_file))
-
-    def __get_selection(self):
-        return np.s_[sum(self.dataset_file_length[0:self.current_dataset]):
-                     sum(self.dataset_file_length[0:self.current_dataset + 1]),
-               (self.current_file * self.number_of_rows):
-               ((self.current_file + 1) * self.number_of_rows)]
-
-    # Override
-    def recommended_chunk_shape(self):
-        return None
-
-    # Override
-    def recommended_data_shape(self):
-        return self.shape
-
-    # Override
-    @property
-    def dtype(self):
-        return np.dtype('int16')
-
-    # Override
-    @property
-    def maxshape(self):
-        return self.shape
