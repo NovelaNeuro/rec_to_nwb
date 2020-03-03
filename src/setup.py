cbc7c0b5a1a86128204c03d6504de487c077432c
wbodo <55714242+wbodo@users.noreply.github.com>
2020-03-03 11:00:12 +0100

    update setup.py

--- src/setup.py
+++ src/setup.py
@@ -1,13 +1,16 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='LorenFranksDataMigration',
-    version='0.0.1',
+    version='0.1.002',
     author='Novela Neurotech',
+    url="https://github.com/NovelaNeuro/lfdatamigration",
+    packages=find_packages(),
     author_email='mbukowski@novelaneuro.com,'
                  ' wmerynda@novelaneuro.com,'
                  ' acwikla@novelaneuro.com,'
                  ' wbodo@novelaneuro.com',
     description='Data transformation from rec binary files into NWB 2.0 format',
     platforms='Posix; MacOS X; Windows',
+python_requires='==3.7.4',
 )
