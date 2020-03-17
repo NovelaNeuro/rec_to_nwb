bce3c721013a6a7928d7e1c2c749457fed90d333
wbodo <55714242+wbodo@users.noreply.github.com>
2020-03-17 13:16:14 +0100

    add xsd files to package data in setup.py (#231)

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     author='Novela Neurotech',
     url="https://github.com/NovelaNeuro/fldatamigration",
     packages=find_packages(),
-    package_data={'': ['logging.conf']},
+    package_data={'': ['logging.conf', 'data/fl_lab_header.xsd', 'data/reconfig_header.xsd']},
     description='Data transformation from rec binary files into NWB 2.0 format',
     platforms='Posix; MacOS X; Windows',
     python_requires='==3.7.4',
