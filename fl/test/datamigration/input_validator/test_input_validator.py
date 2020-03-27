7b565380a7f6ab91074bad4c4caf00d42fe35d2a
wbodo <55714242+wbodo@users.noreply.github.com>
2020-03-27 17:29:50 +0100

    new test_input_validator

--- fl/test/datamigration/input_validator/test_input_validator.py
+++ fl/test/datamigration/input_validator/test_input_validator.py
@@ -1,7 +1,7 @@
 from unittest import TestCase
 
 from fl.datamigration.exceptions.missing_data_exception import MissingDataException
-from fl.datamigration.input_validator import InputValidator
+from fl.datamigration.input_validator.input_validator import InputValidator
 from pathlib import Path
 
 from fl.datamigration.tools.data_scanner import DataScanner
@@ -18,10 +18,8 @@ class TestInputValidator(TestCase):
         data_scanner = DataScanner(data_path, animal)
         self.all_data = data_scanner.get_all_data_from_dataset(date)
 
-
-
     def test_input_validator_validate_input_data_successfully(self):
-        wrong_data_types_to_check = ['pos', 'non_existing']
+        wrong_data_types_to_check = ['pos', 'mda', 'non_existing']
         data_types_to_check = ['pos', 'mda']
         metadata_path = str(path) + '/res/metadata.yml'
         wrong_metadata_path = str(path) + '/res/metadataa.yml'
@@ -31,56 +29,25 @@ class TestInputValidator(TestCase):
         wrong_probes_paths = [str(path) + '/res/probe11.yml',
                               str(path) + '/res/probe22.yml',
                               str(path) + '/res/probe33.yml']
-        validator = InputValidator()
+        validator_missing_type = InputValidator(metadata_path,
+                                                probes_paths,
+                                                self.all_data,
+                                                self.epochs,
+                                                wrong_data_types_to_check)
+        validator_wrong_probes = InputValidator(metadata_path,
+                                                wrong_probes_paths,
+                                                self.all_data,
+                                                self.epochs,
+                                               data_types_to_check)
+        validator_wrong_metadata = InputValidator(wrong_metadata_path,
+                                                  probes_paths,
+                                                  self.all_data,
+                                                  self.epochs,
+                                                  data_types_to_check)
         with self.assertRaises(MissingDataException):
-            validator.validate_input_data(metadata_path,
-                                          probes_paths,
-                                          self.all_data,
-                                          self.epochs,
-                                          wrong_data_types_to_check)
+            validator_missing_type.validate_input_data()
         with self.assertRaises(MissingDataException):
-            validator.validate_input_data(metadata_path,
-                                          wrong_probes_paths,
-                                          self.all_data,
-                                          self.epochs,
-                                          data_types_to_check)
+            validator_wrong_probes.validate_input_data()
         with self.assertRaises(MissingDataException):
-            validator.validate_input_data(wrong_metadata_path,
-                                          probes_paths,
-                                          self.all_data,
-                                          self.epochs,
-                                          data_types_to_check)
-
-
-    def test_input_validator_validate_dataset_successfully(self):
-        data_path = str(path) + '/res/scanner_test/'
-        wrong_data_types_to_check = ['pos', 'non_existing']
-        data_types_to_check = ['pos', 'mda']
-        validator = InputValidator()
-        self.assertEqual(validator.return_missing_preprocessing_data(self.all_data,
-                                                                     self.epochs,
-                                                                     data_types_to_check), '')
-        self.assertEqual(validator.return_missing_preprocessing_data(self.all_data,
-                                                                     self.epochs,
-                                                                     wrong_data_types_to_check),
-                         'non_existing files in epoch 01_s1\nnon_existing files in epoch 02_s1\n')
-
-    def test_input_validator_validate_metadata_successfully(self):
-        validator = InputValidator()
-        metadata_path = str(path) + '/res/metadata.yml'
-        wrong_metadata_path = str(path) + '/res/metadataa.yml'
-        probes_paths = [str(path) + '/res/probe1.yml',
-                        str(path) + '/res/probe2.yml',
-                        str(path) + '/res/probe3.yml']
-        wrong_probes_paths = [str(path) + '/res/probe11.yml',
-                              str(path) + '/res/probe22.yml',
-                              str(path) + '/res/probe33.yml']
-        self.assertEqual(validator.return_missing_metadata(metadata_path, probes_paths), '')
-        self.assertEqual(validator.return_missing_metadata(wrong_metadata_path, probes_paths),
-                         str(path) + '/res/metadataa.yml\n')
-        self.assertEqual(validator.return_missing_metadata(metadata_path, wrong_probes_paths),
-                         str(path) + '/res/probe11.yml\n' +
-                         str(path) + '/res/probe22.yml\n' +
-                         str(path) + '/res/probe33.yml\n')
-
+            validator_wrong_metadata.validate_input_data()
 
