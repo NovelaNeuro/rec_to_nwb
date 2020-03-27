a27441399c1cba8b12e3fbddbdccd87ad863a560
Wojciech Merynda <wmerynda@novelaneuro.com>
2020-03-27 15:51:57 +0100

    test epochs not equal param length

--- fl/datamigration/nwb/components/epochs/fl_epochs_manager.py
+++ fl/datamigration/nwb/components/epochs/fl_epochs_manager.py
@@ -1,3 +1,4 @@
+from fl.datamigration.nwb.components.epochs.epochs_tag_extractor import EpochsTagExtractor
 from fl.datamigration.nwb.components.epochs.fl_epochs_builder import FlEpochsBuilder
 from fl.datamigration.nwb.components.epochs.fl_epochs_extractor import FlEpochsExtractor
 from fl.datamigration.tools.task_names_extractor import TaskNamesExtractor
@@ -10,12 +11,13 @@ class FlEpochsManager:
         validate_parameters_not_none(__name__, datasets, metadata)
 
         self.continuous_time_files = [dataset.get_continuous_time() for dataset in datasets]
-        self.epoch_tags = [dataset.name for dataset in datasets]
         self.task_names_extractor = TaskNamesExtractor(metadata)
+        epochs_tag_extractor = EpochsTagExtractor(datasets)
+        self.epochs_tags = epochs_tag_extractor.get_tags()
 
     def get_epochs(self):
         return FlEpochsBuilder.build(
             FlEpochsExtractor.extract_epochs(self.continuous_time_files),
-            self.epoch_tags,
+            self.epochs_tags,
             self.task_names_extractor.get_task_names()
         )
