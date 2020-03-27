c7906f6dff825a404fc00565b0ae1e90dfa297b0
Wojciech Merynda <wmerynda@novelaneuro.com>
2020-03-27 15:22:00 +0100

    test for injector and fix as coments asked

--- fl/test/datamigration/epoch/test_flEpochsManager.py
+++ fl/test/datamigration/epoch/test_flEpochsManager.py
@@ -4,7 +4,6 @@ from unittest.mock import patch, Mock
 from testfixtures import should_raise
 
 from fl.datamigration.exceptions.none_param_exception import NoneParamException
-from fl.datamigration.nwb.components.epochs.fl_epochs import FlEpochs
 from fl.datamigration.nwb.components.epochs.fl_epochs_extractor import FlEpochsExtractor
 from fl.datamigration.nwb.components.epochs.fl_epochs_manager import FlEpochsManager
 from fl.datamigration.tools.dataset import Dataset
@@ -23,11 +22,11 @@ class TestFlElEpochsManager(TestCase):
     @patch.object(Dataset, 'get_continuous_time', new=fake_get_continuous_time)
     @patch.object(FlEpochsExtractor, 'extract_epochs', new=fake_extract_epochs)
     def test_get_epochs_returnCorrectData_successfully(self):
-        mock1 = Mock(spec=Dataset)
-        mock1.name = 'mock1'
-        mock2 = Mock(spec=Dataset)
-        mock2.name = 'mock2'
-        datasets = [mock1, mock2]
+        dataset_1_mock = Mock(spec=Dataset)
+        dataset_1_mock.name = 'mock1'
+        dataset_2_mock = Mock(spec=Dataset)
+        dataset_2_mock.name = 'mock2'
+        datasets = [dataset_1_mock, dataset_2_mock]
         metadata = {'tasks': [{'task_name': 'task1'}, {'task_name': 'task2'}]}
 
         fl_epochs_manager = FlEpochsManager(
@@ -36,11 +35,14 @@ class TestFlElEpochsManager(TestCase):
         )
         fl_epochs = fl_epochs_manager.get_epochs()
 
-        self.assertIsInstance(fl_epochs, FlEpochs)
         self.assertEqual(2, len(fl_epochs.session_start_times))
         self.assertEqual(2, len(fl_epochs.session_end_times))
         self.assertEqual(2, len(fl_epochs.tags))
         self.assertEqual(2, len(fl_epochs.tasks))
+        self.assertEqual(11111111.0, fl_epochs.session_start_times[0])
+        self.assertEqual(4444444444.0, fl_epochs.session_end_times[1])
+        self.assertEqual('task1', fl_epochs.tasks[0])
+        self.assertEqual('mock1', fl_epochs.tags[0])
 
     @should_raise(NoneParamException)
     def test_get_epochs_fails_due_to_None_param(self):
