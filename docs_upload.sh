aeb122dd201462dba15bf5e321f1b1ddf502bd47
acwikla <acwikla@novelaneuro.com>
2020-03-30 11:08:01 +0200

    Doc finish

--- docs_upload.sh
+++ docs_upload.sh
@@ -1,23 +1,34 @@
 DOC_REPO='https://github.com/NovelaNeuro/fldatamigration-docs'
+GH_DOCS_TOKEN='d3fd571a862f479ee606ff79e34b26e588002fe2'
+USERNAME='acwikla-novela'
 
 echo 'README.md copying to docs/source and removing first line'
-tail -n +2 README.md > README.tmp && mv README.tmp docs/source/README.md
+tail -n +2 README.md > README.tmp && mv README.tmp docs/source/README.md || exit 1
 
 echo 'LICENSE.md copying to docs/source and adding first line'
-cp LICENSE.md ./docs/source/
-sed -i '1i License' ./docs/source/LICENSE.md && sed -i '2i ===================' ./docs/source/LICENSE.md
+cp LICENSE.md ./docs/source/ || exit 1
+sed -i '1i License' ./docs/source/LICENSE.md && sed -i '2i ===================' ./docs/source/LICENSE.md || exit 1
 
 echo 'Creating API Documentation'
-sphinx-apidoc -fMET ./fl/datamigration -o ./docs/source/autoapi
+sphinx-apidoc -fMET ./fl/datamigration -o ./docs/source/autoapi || exit 1
 
 echo 'Create HTML Documentation'
-make html -C ./docs
+make html -C ./docs || exit 1
 
-#echo 'Deploy documentation to' $DOC_REPO docs/source
+echo 'Deploy documentation to' $DOC_REPO
+git clone ${DOC_REPO}
+cd fldatamigration-docs
+git remote rm origin
+git remote add origin https://${USERNAME}:${GH_DOCS_TOKEN}@github.com/NovelaNeuro/fldatamigration-docs.git
+git add -f ./docs/html
+git commit -m "Documentation v$TRAVIS_BUILD_NUMBER"
+git push -f origin master
+
+
 # maybe this? https://github.community/t5/How-to-use-Git-and-GitHub/Adding-a-folder-from-one-repo-to-another/td-p/5425
 # git subtree push --prefix=docs\source &DOC_REPO master
 # https://gist.github.com/Maumagnaguagno/84a9807ed71d233e5d3f
 
-
-echo 'Click enter to exit'
-read exitKey
+#ToDo to delete
+#echo 'Click enter to exit'
+#read exitKey
