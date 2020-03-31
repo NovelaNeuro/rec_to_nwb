2d43ae13d44f1f6820be5d9db4fe06d4377c99ed
Wojciech Merynda <48327847+WojciechMerynda@users.noreply.github.com>
2020-03-31 13:55:23 +0200

    adjust validator task (#252)

--- fl/datamigration/validation/task_validation_summary.py
+++ fl/datamigration/validation/task_validation_summary.py
@@ -1,10 +1,14 @@
-class TaskValidator:
+from fl.datamigration.validation.validation_summary import ValidationSummary
+
+
+class TaskValidationSummary(ValidationSummary):
 
     def __init__(self, datasets, tasks):
         self.datasets = datasets
         self.tasks = tasks
 
-    def is_number_of_tasks_valid(self):
+    def isValid(self):
         number_of_epochs = len(self.datasets)
         number_of_tasks = len(self.tasks)
         return number_of_epochs == number_of_tasks
+
