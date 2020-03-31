01d045cf9e1e37449c1c8127005b080a9aca2993
acwikla <acwikla@novelaneuro.com>
2020-03-31 14:53:54 +0200

    Comment deploy

--- docs_upload.sh
+++ docs_upload.sh
@@ -1,6 +1,6 @@
 DOC_REPO='https://github.com/NovelaNeuro/fldatamigration-docs'
-GH_DOCS_TOKEN='d3fd571a862f479ee606ff79e34b26e588002fe2'
-USERNAME='acwikla-novela'
+#GH_DOCS_TOKEN='d3fd571a862f479ee606ff79e34b26e588002fe2'
+##USERNAME='acwikla-novela'
 
 echo 'README.md copying to docs/source and removing first line'
 tail -n +2 README.md > README.tmp && mv README.tmp docs/source/README.md || exit 1
@@ -15,16 +15,16 @@ sphinx-apidoc -fMET ./fl/datamigration -o ./docs/source/autoapi || exit 1
 echo 'Create HTML Documentation'
 make html -C ./docs || exit 1
 
-echo 'Deploy documentation to' $DOC_REPO
-git clone ${DOC_REPO}
-cd fldatamigration-docs
-ls ../
-git remote rm origin
-git remote add origin https://${USERNAME}:${GH_DOCS_TOKEN}@github.com/NovelaNeuro/fldatamigration-docs.git
-cp -r ../docs/build/html .
-git add -f .
-git commit -m "Documentation v$TRAVIS_BUILD_NUMBER"
-git push -f origin master
+#echo 'Deploy documentation to' $DOC_REPO
+#git clone ${DOC_REPO}
+#cd fldatamigration-docs
+#ls ../
+#git remote rm origin
+#git remote add origin https://${USERNAME}:${GH_DOCS_TOKEN}@github.com/NovelaNeuro/fldatamigration-docs.git
+#cp -r ../docs/build/html .
+#git add -f .
+#git commit -m "Documentation v$TRAVIS_BUILD_NUMBER"
+#git push -f origin master
 
 
 # maybe this? https://github.community/t5/How-to-use-Git-and-GitHub/Adding-a-folder-from-one-repo-to-another/td-p/5425
@@ -32,5 +32,5 @@ git push -f origin master
 # https://gist.github.com/Maumagnaguagno/84a9807ed71d233e5d3f
 
 #ToDo to delete
-#echo 'Click enter to exit'
-#read exitKey
+echo 'Click enter to exit'
+read exitKey
