a2b684b19ed33ffd58520618efbabfa3fb7e9f93
wbodo <55714242+wbodo@users.noreply.github.com>
2020-03-31 15:31:02 +0200

    fix test task validator

--- fl/test/datamigration/test_taskValidator.py
+++ fl/test/datamigration/test_taskValidator.py
@@ -8,21 +8,17 @@ from fl.datamigration.validation.task_validator import TaskValidator
 class TestTaskValidator(TestCase):
 
     def setUp(self):
-        dataset_1_mock = Mock(spec=Dataset)
-        dataset_1_mock.name = 'mock1'
-        dataset_2_mock = Mock(spec=Dataset)
-        dataset_2_mock.name = 'mock2'
-        self.datasets = [dataset_1_mock, dataset_2_mock]
+        self.number_of_datasets = 2
 
     def test_task_validator_equal_number_of_task_and_epochs_valid(self):
         tasks = [{'task_name': 'task1'}, {'task_name': 'task2'}]
-        task_validator = TaskValidator(self.datasets, tasks)
+        task_validator = TaskValidator(self.number_of_datasets, tasks)
         result = task_validator.createSummary()
         self.assertTrue(result.isValid())
 
     def test_task_validator_different_number_of_tasks_and_epochs_failed(self):
         tasks = [{'task_name': 'task1'}, {'task_name': 'task2'}, {'task_name': 'task3'}]
-        task_validator = TaskValidator(self.datasets, tasks)
+        task_validator = TaskValidator(self.number_of_datasets, tasks)
         result = task_validator.createSummary()
         self.assertFalse(result.isValid())
 
