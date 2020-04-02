c03db281e17c2d654bb7170d8f99709af9bd9260
acwikla <acwikla@novelaneuro.com>
2020-04-02 14:46:21 +0200

    Anaconda upload Travis fix

--- conda_upload.sh
+++ conda_upload.sh
@@ -9,16 +9,14 @@ echo "Building conda package..."
 conda build . || exit 1
 export CONDA_BUILD_PATH=/home/travis/miniconda/envs/test-environment/conda-bld
 
-#ToDo uncomment and delete read exitKey
-#echo "Converting conda package..."
-#conda convert --platform osx-64 $CONDA_BUILD_PATH/linux-64/***.tar.bz2 --output-dir $CONDA_BUILD_PATH -q || exit 1
-#conda convert --platform linux-32 $CONDA_BUILD_PATH/linux-64/***.tar.bz2 --output-dir $CONDA_BUILD_PATH -q || exit 1
-#conda convert --platform linux-64 $CONDA_BUILD_PATH/linux-64/***.tar.bz2 --output-dir $CONDA_BUILD_PATH -q || exit 1
-#conda convert --platform win-32 $CONDA_BUILD_PATH/linux-64/***.tar.bz2 --output-dir $CONDA_BUILD_PATH -q || exit 1
-#conda convert --platform win-64 $CONDA_BUILD_PATH/linux-64/***.tar.bz2 --output-dir $CONDA_BUILD_PATH -q || exit 1
-#
-#echo "Deploying to Anaconda.org..."
-#anaconda upload $CONDA_BUILD_PATH/**/fldatamigration-*.tar.bz2 --force || exit 1
-#
+echo "Converting conda package..."
+conda convert --platform osx-64 $CONDA_BUILD_PATH/linux-64/***.tar.bz2 --output-dir $CONDA_BUILD_PATH -q || exit 1
+conda convert --platform linux-32 $CONDA_BUILD_PATH/linux-64/***.tar.bz2 --output-dir $CONDA_BUILD_PATH -q || exit 1
+conda convert --platform linux-64 $CONDA_BUILD_PATH/linux-64/***.tar.bz2 --output-dir $CONDA_BUILD_PATH -q || exit 1
+conda convert --platform win-32 $CONDA_BUILD_PATH/linux-64/***.tar.bz2 --output-dir $CONDA_BUILD_PATH -q || exit 1
+conda convert --platform win-64 $CONDA_BUILD_PATH/linux-64/***.tar.bz2 --output-dir $CONDA_BUILD_PATH -q || exit 1
 
-read exitKey
+echo "Deploying to Anaconda.org..."
+anaconda upload $CONDA_BUILD_PATH/**/fldatamigration-*.tar.bz2 --force || exit 1
+
+
