128428b56d7e02ae0932b8ec27f6e5c590d3f229
Wojciech Merynda <wmerynda@novelaneuro.com>
2020-04-06 11:13:26 +0200

    change  task validator

--- fl/datamigration/validation/task_validation_summary.py
+++ fl/datamigration/validation/task_validation_summary.py
@@ -3,12 +3,9 @@ from fl.datamigration.validation.validation_summary import ValidationSummary
 
 class TaskValidationSummary(ValidationSummary):
 
-    def __init__(self, number_of_epochs, tasks):
-        self.number_of_epochs = number_of_epochs
+    def __init__(self, tasks):
         self.tasks = tasks
 
     def isValid(self):
-        number_of_epochs = self.number_of_epochs
-        number_of_tasks = len(self.tasks)
-        return number_of_epochs == number_of_tasks
+        return type(self.tasks) == list
 
