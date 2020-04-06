7985e17a5dcde58990032a4922403b6c3be54ef9
Wojciech Merynda <wmerynda@novelaneuro.com>
2020-04-06 11:48:03 +0200

    extract analog default value in readme

--- fl/datamigration/nwb/components/epochs/epochs_injector.py
+++ fl/datamigration/nwb/components/epochs/epochs_injector.py
@@ -6,11 +6,4 @@ class EpochsInjector:
             nwb_content.add_epoch(
                 fl_epochs.session_start_times[i],
                 fl_epochs.session_end_times[i],
-                fl_epochs.tags[i],
             )
-        EpochsInjector.__extend_epochs(fl_epochs, nwb_content)
-
-    @staticmethod
-    def __extend_epochs(fl_epochs, nwb_content):
-        tasks = fl_epochs.tasks
-        nwb_content.add_epoch_column('tasks', 'None', data=tasks)
--- fl/datamigration/nwb/components/epochs/fl_epochs.py
+++ fl/datamigration/nwb/components/epochs/fl_epochs.py
@@ -3,10 +3,9 @@ from fl.datamigration.tools.validate_parameters import validate_parameters_equal
 
 class FlEpochs:
 
-    def __init__(self, session_start_times, session_end_times, tags, tasks):
-        validate_parameters_equal_length(__name__, session_start_times, session_end_times, tags, tasks)
+    def __init__(self, session_start_times, session_end_times, tags):
+        validate_parameters_equal_length(__name__, session_start_times, session_end_times, tags)
 
         self.session_start_times = session_start_times
         self.session_end_times = session_end_times
         self.tags = tags
-        self.tasks = tasks
