a367ad3c568921de60df7eff790767cc0d6f847b
wbodo <55714242+wbodo@users.noreply.github.com>
2020-04-14 14:37:45 +0200

    cleanup

--- fl/datamigration/nwb/components/invalid_times/invalid_time_injector.py
+++ fl/datamigration/nwb/components/invalid_times/invalid_time_injector.py
@@ -3,10 +3,11 @@ class InvalidTimeInjector:
 
     @staticmethod
     def inject_mda_invalid_times(gaps, nwb_content):
-        """insert electrical series to nwb file"""
-        nwb_content.create_time_intervals(name='Valid Times',
-                                          description='Some Description',
-                                          )
+        """insert invalid times to nwb file"""
+        nwb_content.create_time_intervals(
+            name='Valid Times',
+            description='Some Description',
+        )
         for gap in gaps:
             nwb_content.add_invalid_time_interval(start_time=gap[0], stop_time=gap[1])
 
