e6aaf5606c43fbb09f13a8b8406daba3d728a5cb
acwikla <55695978+acwikla-novela@users.noreply.github.com>
2020-04-15 12:29:54 +0200

    user/acwikla/one_probe_per_type (#296)

--- fl/datamigration/nwb/components/electrode_group/fl_electrode_group_manager.py
+++ fl/datamigration/nwb/components/electrode_group/fl_electrode_group_manager.py
@@ -10,7 +10,19 @@ class FlElectrodeGroupManager:
     def get_fl_electrode_groups(self, probes):
         validate_parameters_not_none(__name__, self.electrode_groups_metadata, probes)
 
-        return [FlElectrodeGroup(
-            metadata=electrode_group_metadata,
-            device=probes[counter]
-        ) for counter, electrode_group_metadata in enumerate(self.electrode_groups_metadata)]
+        fl_electrode_groups = []
+        for electrode_group_metadata in self.electrode_groups_metadata:
+            probe = self.__get_probe_by_type(probes, electrode_group_metadata['device_type'])
+            fl_electrode_groups.append(
+                FlElectrodeGroup(
+                    metadata=electrode_group_metadata,
+                    device=probe
+                )
+            )
+        return fl_electrode_groups
+
+    @staticmethod
+    def __get_probe_by_type(probes, probe_type):
+        for probe in probes:
+            if probe_type == probe.probe_type:
+                return probe
