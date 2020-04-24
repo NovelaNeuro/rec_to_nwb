6e1a826e88ae59102995eb020a351cf15f754482
acwikla <acwikla@novelaneuro.com>
2020-04-24 15:06:35 +0200

    change method for type validation from pyvalid to beartype add beartype wrapper

--- fl/test/e2etests/test_rawToNwbGeneration.py
+++ fl/test/e2etests/test_rawToNwbGeneration.py
@@ -1,6 +1,8 @@
 import os
 import unittest
 
+from testfixtures import should_raise
+
 from fl.datamigration.metadata.metadata_manager import MetadataManager
 from fl.datamigration.raw_to_nwb_builder import RawToNWBBuilder
 
@@ -13,7 +15,7 @@ _DEFAULT_ANALOG_EXPORT_ARGS = ('-reconfig', str(path) + '/../datamigration/res/r
 class TestRawToNWBGeneration(unittest.TestCase):
 
     def setUp(self):
-        metadata = MetadataManager(
+        self.metadata = MetadataManager(
             str(path) + '/../datamigration/res/metadata.yml',
             [
                 str(path) + '/../datamigration/res/probe1.yml',
@@ -25,7 +27,7 @@ class TestRawToNWBGeneration(unittest.TestCase):
             animal_name='beans',
             data_path=str(path) + '/../test_data/',
             dates=['20190718'],
-            nwb_metadata=metadata,
+            nwb_metadata=self.metadata,
             output_path='',
             extract_spikes=False,
             extract_mda=True,
@@ -39,6 +41,32 @@ class TestRawToNWBGeneration(unittest.TestCase):
     def test_from_raw_to_nwb_generation(self):
         self.builder.build_nwb()
         self.assertTrue(os.path.exists('beans20190718.nwb'), 'NWBFile did not build')
+
+    @should_raise(TypeError)
+    def test_raw_to_nwb_builder_failed_due_to_none_parameters(self):
+        RawToNWBBuilder(
+            animal_name='beans',
+            data_path=str(path) + '/../test_data/',
+            dates=['20190718'],
+            nwb_metadata=None
+        )
+
+    @should_raise(TypeError)
+    def test_raw_to_nwb_builder_failed_due_to_incorrect_type_parameters(self):
+        RawToNWBBuilder(
+            animal_name=11111,
+            data_path=str(path) + '/../test_data/',
+            dates=['20190718'],
+            nwb_metadata=self.metadata,
+            output_path='',
+            extract_spikes=False,
+            extract_mda=True,
+            extract_lfps=False,
+            extract_analog=True,
+            extract_dio=True,
+            overwrite=True,
+            analog_export_args=_DEFAULT_ANALOG_EXPORT_ARGS
+        )
 
     def tearDown(self):
         self.builder.cleanup()
