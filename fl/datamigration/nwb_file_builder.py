3cdf4909dfc15d36d78ccf8bf4fb123c2e806bb5
acwikla <acwikla@novelaneuro.com>
2020-04-27 11:20:46 +0200

    nwb_file_builder fix

--- fl/datamigration/nwb_file_builder.py
+++ fl/datamigration/nwb_file_builder.py
@@ -290,22 +290,10 @@ class NWBFileBuilder:
 
     def __build_shanks_electrodes(self):
         logger.info('Probes-ShanksElectrode: Building')
-        fl_shanks_electrodes_dict = self.fl_shanks_electrode_manager.get_fl_shanks_electrodes()
-        logger.info('Probes-ShanksElectrode: Creating')
-        shanks_electrodes_dict = {}
-        for probe_type, fl_shanks_electrodes in fl_shanks_electrodes_dict:
-            shanks_electrodes_dict[probe_type] = [
-                self.shanks_electrodes_creator.create(fl_shanks_electrode)
-                for fl_shanks_electrode in fl_shanks_electrodes
-            ]
-        return shanks_electrodes_dict
-
-    def __build_shanks_electrodes(self):
-        logger.info('Probes-ShanksElectrode: Building')
         fl_shanks_electrodes_dict = self.fl_shanks_electrode_manager.get_fl_shanks_electrodes_dict()
         logger.info('Probes-ShanksElectrode: Creating')
         shanks_electrodes_dict = {}
-        for probe_type, fl_shanks_electrodes in fl_shanks_electrodes_dict.items():
+        for probe_type, fl_shanks_electrodes in fl_shanks_electrodes_dict:
             shanks_electrodes_dict[probe_type] = [
                 self.shanks_electrodes_creator.create(fl_shanks_electrode)
                 for fl_shanks_electrode in fl_shanks_electrodes
