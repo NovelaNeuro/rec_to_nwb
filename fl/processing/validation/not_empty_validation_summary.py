fba92c9e6cab46c4ebbf11ffb76b4b679ddf4bb1
wbodo <55714242+wbodo@users.noreply.github.com>
2020-05-06 14:37:40 +0200

    fix

---
+++ fl/processing/validation/not_empty_validation_summary.py
@@ -0,0 +1,14 @@
+from fl.processing.validation.validation_summary import ValidationSummary
+
+
+class NotEmptyValidationSummary(ValidationSummary):
+
+    def __init__(self, parameter):
+        self.parameter = parameter
+
+    def is_valid(self):
+        if self.parameter:
+            return True
+        else:
+            return False
+
