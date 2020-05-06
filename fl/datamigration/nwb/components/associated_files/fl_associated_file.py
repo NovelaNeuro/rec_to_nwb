caf34f35337c45a3fc8d6dc78622c264ad0370e1
Wojciech Merynda <48327847+WojciechMerynda@users.noreply.github.com>
2020-05-06 12:33:51 +0200

    associated files extension (#325)

---
+++ fl/datamigration/nwb/components/associated_files/fl_associated_file.py
@@ -0,0 +1,10 @@
+from fl.datamigration.tools.beartype.beartype import beartype
+
+
+class FlAssociatedFile:
+
+    @beartype
+    def __init__(self, name: str, description: str, content: str):
+        self.name = name
+        self.description = description
+        self.content = content
---
+++ fl/datamigration/nwb/components/associated_files/fl_associated_files_manager.py
@@ -0,0 +1,23 @@
+from fl.datamigration.nwb.components.associated_files.fl_associated_files_builder import FlAssociatedFilesBuilder
+from fl.datamigration.nwb.components.associated_files.fl_associated_files_extractor import FlAssociatedFilesExtractor
+from fl.datamigration.tools.beartype.beartype import beartype
+
+
+class FlAssociatedFilesManager:
+
+    @beartype
+    def __init__(self, files: list, files_metadata: list):
+        self.files_metadata = files_metadata
+        self.fl_associated_files_extractor = FlAssociatedFilesExtractor(files)
+        self.fl_associated_files_builder = FlAssociatedFilesBuilder()
+
+    def get_fl_associated_files(self):
+        return [
+            self.fl_associated_files_builder.build(
+                file['name'],
+                file['description'],
+                self.fl_associated_files_extractor.extract()[i]
+            )
+            for i, file in enumerate(self.files_metadata)
+        ]
+
