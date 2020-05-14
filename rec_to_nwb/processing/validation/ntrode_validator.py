b5c571f3d5c64f4e8613c17c2b10da4644d05ec3
acwikla <acwikla@novelaneuro.com>
2020-05-14 15:29:13 +0200

    Ntrodes validation with probes metadata

--- rec_to_nwb/processing/validation/ntrode_validator.py
+++ rec_to_nwb/processing/validation/ntrode_validator.py
@@ -1,9 +1,14 @@
 import logging.config
 import os
 
-from rec_to_nwb.processing.validation.ntrode_validation_summary import NTrodeValidationSummary
 from rec_to_nwb.processing.exceptions.invalid_header_exception import InvalidHeaderException
 from rec_to_nwb.processing.exceptions.invalid_metadata_exception import InvalidMetadataException
+from rec_to_nwb.processing.header.module.header import Header
+from rec_to_nwb.processing.tools.beartype.beartype import beartype
+from rec_to_nwb.processing.tools.count_electrodes_in_ntrode import count_electrodes_in_ntrode
+from rec_to_nwb.processing.tools.count_electrodes_in_probe import count_electrodes_in_probe
+from rec_to_nwb.processing.tools.filter_probe_by_type import filter_probe_by_type
+from rec_to_nwb.processing.validation.ntrode_validation_summary import NTrodeValidationSummary
 from rec_to_nwb.processing.validation.validator import Validator
 
 path = os.path.dirname(os.path.abspath(__file__))
@@ -14,15 +19,18 @@ logger = logging.getLogger(__name__)
 
 class NTrodeValidator(Validator):
 
-    def __init__(self, metadata, header):
+    @beartype
+    def __init__(self, metadata: dict, header: Header, probes_metadata: list):
         self.metadata = metadata
         self.header = header
+        self.probes_metadata = probes_metadata
 
     def create_summary(self):
         ntrodes = self.metadata['ntrode electrode group channel map']
         if len(ntrodes) == 0:
             raise InvalidMetadataException("There are no ntrodes defined in metadata.yml file.")
-        if self.header is None or \
+        if \
+                self.header is None or \
                 self.header.configuration.spike_configuration is None or \
                 self.header.configuration.spike_configuration.spike_n_trodes is None:
             raise InvalidHeaderException("Rec header does not contain spike_n_trodes data")
@@ -30,4 +38,25 @@ class NTrodeValidator(Validator):
         spike_ntrodes = self.header.configuration.spike_configuration.spike_n_trodes
         ntrodes_num = len(ntrodes)
         spike_ntrodes_num = len(spike_ntrodes)
+
+        self.validate_ntrode_metadata_with_probe_metadata(self.metadata, self.probes_metadata)
+
         return NTrodeValidationSummary(ntrodes_num, spike_ntrodes_num)
+
+    @staticmethod
+    def validate_ntrode_metadata_with_probe_metadata(metadata, probes_metadata):
+        for electrode_group in metadata['electrode groups']:
+            probe_metadata = filter_probe_by_type(probes_metadata, electrode_group['device_type'])
+            electrodes_in_probe = count_electrodes_in_probe(probe_metadata)
+            electrodes_in_group = count_electrodes_in_ntrode(
+                metadata['ntrode electrode group channel map'],
+                electrode_group['id']
+            )
+            if electrodes_in_probe != electrodes_in_group:
+                raise InvalidMetadataException(
+                    'Incomatible ntrode_metadata to probe_schema.' +
+                    'Probe_type: ' + str(electrode_group['device_type']) +
+                    ' electrodes in this probe_type: ' + str(electrodes_in_probe) +
+                    '. Ntrode_metadata for electrode_group of id: ' + str(electrode_group['id']) +
+                    ' electrodes in this electrode_group: ' + str(electrodes_in_group)
+                )
--- rec_to_nwb/test/processing/validators/test_validationRegistrator.py
+++ rec_to_nwb/test/processing/validators/test_validationRegistrator.py
@@ -1,21 +1,23 @@
 from unittest import TestCase
+from unittest.mock import Mock
 
+from rec_to_nwb.processing.header.module.header import Header
 from rec_to_nwb.processing.tools.dataset import Dataset
-from rec_to_nwb.processing.validation.validation_registrator import ValidationRegistrator
 from rec_to_nwb.processing.validation.ntrode_validator import NTrodeValidator
+from rec_to_nwb.processing.validation.validation_registrator import ValidationRegistrator
 
 
 class TestValidationRegistrator(TestCase):
 
     def test_registers_validators_correctly(self):
         validationRegistrator = ValidationRegistrator()
-        validationRegistrator.register(NTrodeValidator(None, None))
-        validationRegistrator.register(NTrodeValidator(None, None))
+        validationRegistrator.register(NTrodeValidator(dict(), Mock(spec=Header), list()))
+        validationRegistrator.register(NTrodeValidator(dict(), Mock(spec=Header), list()))
         self.assertEqual(2, len(validationRegistrator.validators))
 
     def test_registers_only_validators_correctly(self):
         validationRegistrator = ValidationRegistrator()
-        validationRegistrator.register(NTrodeValidator(None, None))
+        validationRegistrator.register(NTrodeValidator(dict(), Mock(spec=Header), list()))
         validationRegistrator.register(None)
         validationRegistrator.register(Dataset(name="name"))
 
