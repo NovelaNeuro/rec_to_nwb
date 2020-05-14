f1af48e05157f34412dd4c0cf51e3e2fbcab1ec6
acwikla <acwikla@novelaneuro.com>
2020-05-14 09:11:15 +0200

    conda script

--- rec_to_nwb/scripts/test_package/create_conda.sh
+++ rec_to_nwb/scripts/test_package/create_conda.sh
@@ -2,12 +2,6 @@
 source ~/miniconda3/etc/profile.d/conda.sh
 
 conda deactivate
-
 conda create -p ./env -y --force
-
-
 conda activate ./env
-#conda install rec_to_nwb -c novelakrk
-conda install fldatamigration -c novelakrk -y
-
-conda list
+conda install rec_to_nwb -c novelakrk
