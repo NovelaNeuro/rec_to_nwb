fdad3934e586554fe6fc268fd948a76027ffa122
acwikla <acwikla@novelaneuro.com>
2020-05-14 09:41:17 +0200

    script

--- rec_to_nwb/scripts/test_package/test_package_after_release.py
+++ rec_to_nwb/scripts/test_package/test_package_after_release.py
@@ -1,4 +1,5 @@
 import os
+from pathlib import Path
 from unittest import TestCase
 
 import pynwb
@@ -6,10 +7,11 @@ import pynwb
 from rec_to_nwb.processing.metadata.metadata_manager import MetadataManager
 from rec_to_nwb.processing.raw_to_nwb_builder import RawToNWBBuilder
 
-path = os.path.dirname(os.path.abspath(__file__))
+path = Path(__file__).parent.parent
+path.resolve()
 _DEFAULT_ANALOG_EXPORT_ARGS = (
     '-reconfig',
-    str(path) + '../rec_to_nwb/rec_to_nwb/test/processing/res/reconfig_header.xml'
+    str(path) + '/rec_to_nwb/rec_to_nwb/test/processing/res/reconfig_header.xml'
 )
 
 
@@ -26,17 +28,17 @@ class TestPackageAfterRelease(TestCase):
         os.system('create_conda.sh')
 
     def test_setUpInterpreter(self):
-        print(str(path) + '\env\python.exe')
+        print(str(path) + '\\test_package\env\python.exe')
 
     def test_build_and_read_nwb(self):
         metadata = MetadataManager(
-            str(path) + '/../rec_to_nwb/rec_to_nwb/test/processing/res/metadata.yml',
-            [str(path) + '/../rec_to_nwb/rec_to_nwb/test/processing/res/probe1.yml',
-             str(path) + '/../rec_to_nwb/rec_to_nwb/test/processing/res/probe2.yml',
-             str(path) + '/../rec_to_nwb/rec_to_nwb/test/processing/res/probe3.yml'])
+            str(path) + '/rec_to_nwb/rec_to_nwb/test/processing/res/metadata.yml',
+            [str(path) + '/rec_to_nwb/rec_to_nwb/test/processing/res/probe1.yml',
+             str(path) + '/rec_to_nwb/rec_to_nwb/test/processing/res/probe2.yml',
+             str(path) + '/rec_to_nwb/rec_to_nwb/test/processing/res/probe3.yml'])
         builder = RawToNWBBuilder(
             animal_name='beans',
-            data_path='/../rec_to_nwb/rec_to_nwb/test/test_data/',
+            data_path=str(path) + '/rec_to_nwb/rec_to_nwb/test/test_data/',
             dates=['20190718'],
             nwb_metadata=metadata,
             output_path='',
