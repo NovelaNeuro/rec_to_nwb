92e4c381c39ba6c85eeb94c4f2db9350ecfb0c86
acwikla <acwikla@novelaneuro.com>
2020-05-21 12:14:07 +0200

    cleanup

--- rec_to_nwb/processing/nwb_file_builder.py
+++ rec_to_nwb/processing/nwb_file_builder.py
@@ -302,14 +302,14 @@ class NWBFileBuilder:
         if self.process_mda:
             self.__build_and_inject_mda(nwb_content)
 
+            if self.process_mda_valid_times:
+                self.__build_and_inject_mda_valid_times(nwb_content)
+
+            if self.process_mda_invalid_times:
+                self.__build_and_inject_mda_invalid_times(nwb_content)
+
         if self.process_analog:
             self.__build_and_inject_analog(nwb_content)
-
-        if self.process_mda_valid_times:
-            self.__build_and_inject_mda_valid_times(nwb_content)
-
-        if self.process_mda_invalid_times:
-            self.__build_and_inject_mda_invalid_times(nwb_content)
 
         if self.process_pos_valid_times:
             self.__build_and_inject_pos_valid_times(nwb_content)
