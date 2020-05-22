6ae2847b15bdccdb8498420ffe463c950801908b
wbodo <55714242+wbodo@users.noreply.github.com>
2020-05-22 12:34:02 +0200

    add labels from pos

--- rec_to_nwb/processing/nwb/components/position/fl_position_builder.py
+++ rec_to_nwb/processing/nwb/components/position/fl_position_builder.py
@@ -4,5 +4,5 @@ from rec_to_nwb.processing.nwb.components.position.fl_position import FlPosition
 class FlPositionBuilder:
 
     @staticmethod
-    def build(position_data, timestamps, conversion):
-        return FlPosition(position_data, timestamps, conversion)
+    def build(position_data, column_labels, timestamps, conversion):
+        return FlPosition(position_data, column_labels, timestamps, conversion)
--- rec_to_nwb/processing/nwb/components/position/pos_data_manager.py
+++ rec_to_nwb/processing/nwb/components/position/pos_data_manager.py
@@ -13,14 +13,22 @@ class PosDataManager(DataManager):
         """extract data from POS files and build FlPos"""
         pos_online = readTrodesExtractedDataFile(self.directories[dataset_id][file_id])
         position = pd.DataFrame(pos_online['data'])
-        labels = self.get_column_labels(dataset_id, file_id)
+        labels = self.get_column_labels()
         filtered_position = [position[label] for label in labels]
         return filtered_position
 
-    def get_column_labels(self, dataset_id, file_id):
+    def get_column_labels(self):
         """extract column labels from POS files"""
-        pos_online = readTrodesExtractedDataFile(self.directories[dataset_id][file_id])
+        pos_online = readTrodesExtractedDataFile(self.directories[0][0])
         column_labels = pd.DataFrame(pos_online['data']).columns
         column_labels = column_labels[1:]
         column_labels_list = [label for label in column_labels]
         return column_labels_list
+
+    def get_column_labels_as_string(self):
+        """extract column labels from POS files and converts them do single string"""
+        labels = self.get_column_labels()
+        labels_string = ''
+        for label in labels:
+            labels_string += label + ', '
+        return labels_string
