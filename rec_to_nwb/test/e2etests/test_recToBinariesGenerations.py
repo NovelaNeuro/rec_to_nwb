9095036c2ba239ce9992cd7b8d9925170a47f829
acwikla <acwikla@novelaneuro.com>
2020-05-26 13:04:05 +0200

    Argument name fix

--- rec_to_nwb/test/e2etests/test_recToBinariesGenerations.py
+++ rec_to_nwb/test/e2etests/test_recToBinariesGenerations.py
@@ -13,11 +13,11 @@ class TestRecToBinGeneration(unittest.TestCase):
     @unittest.skip("Super heavy REC to Preprocessing Generation")
     def test_generation_preprocessing(self):
         extract_trodes_rec_file(
-                data_dir='../test_data/',
-                animal='beans',
-                parallel_instances=4,
-            trodes_rec_export_args=_DEFAULT_TRODES_REC_EXPORT_ARGS,
-                overwrite=True,
+            data_dir='../test_data/',
+            animal='beans',
+            parallel_instances=4,
+            analog_export_args=_DEFAULT_TRODES_REC_EXPORT_ARGS,
+            overwrite=True,
         )
 
         self.assertTrue(os.path.isdir('/../test_data/beans/preprocessing'))
