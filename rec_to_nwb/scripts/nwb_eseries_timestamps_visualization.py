f34c86799bbd0f110ec267a9494799213c6cc0c1
Wojciech Merynda <48327847+WojciechMerynda@users.noreply.github.com>
2020-05-27 12:45:38 +0200

    adjust to new ndx releae v0.0.004 (#394)

--- rec_to_nwb/scripts/nwb_eseries_timestamps_visualization.py
+++ rec_to_nwb/scripts/nwb_eseries_timestamps_visualization.py
@@ -2,9 +2,9 @@
 import matplotlib.pyplot as plt
 from pynwb import NWBHDF5IO
 
-from ndx_fl_novela.apparatus import Apparatus, Edge, Node
-from ndx_fl_novela.header_device import HeaderDevice
-from ndx_fl_novela.ntrode import NTrode
+from ndx_franklab_novela.apparatus import Apparatus, Edge, Node
+from ndx_franklab_novela.header_device import HeaderDevice
+from ndx_franklab_novela.ntrode import NTrode
 
 nwb_file = NWBHDF5IO('rec_to_nwb/rec_to_nwb/test/beans20190718.nwb', 'r')
 nwbfile_read = nwb_file.read()
