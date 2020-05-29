aee204f385b355c20a186692e8d130b4b2357773
Wojciech Merynda <wmerynda@novelaneuro.com>
2020-05-29 10:06:55 +0200

    clean repo

--- rec_to_nwb/processing/exceptions/different_number_of_tasks_and_epochs_exception.py
+++
@@ -1,2 +0,0 @@
-class DifferentNumberOfTasksAndEpochsException(Exception):
-    pass
--- rec_to_nwb/processing/tools/task_names_extractor.py
+++
@@ -1,8 +0,0 @@
-class TaskNamesExtractor:
-
-    def __init__(self, tasks):
-        self.tasks = tasks
-
-    def get_task_names(self):
-        return [task_dict['task_name'] for task_dict in self.tasks]
-
