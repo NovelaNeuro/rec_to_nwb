f0acfb537f71eeaa0f8676dd97da58c57bc7210c
acwikla <acwikla@novelaneuro.com>
2020-06-01 18:25:54 +0200

    Test conda package by travis

--- conda_upload.sh
+++ conda_upload.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export PKG_NAME=rec_to_nwb
-export ANACONDA_API_TOKEN=$CONDA_UPLOAD_TEST_TOKEN
+export ANACONDA_API_TOKEN=$CONDA_UPLOAD_TOKEN
 export VERSION=$(python setup.py)
 export CONDA_BUILD_PATH=/home/travis/miniconda/envs/test-environment/conda-bld
 export BASE_PATH=$(pwd)
