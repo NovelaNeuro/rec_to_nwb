afec8a5e475cc865d7c765d7aef4605a5db7725e
acwikla <acwikla@novelaneuro.com>
2020-06-02 08:58:58 +0200

    ElectrodeGroupInjector

--- rec_to_nwb/processing/nwb/components/electrode_group/electrode_group_injector.py
+++ rec_to_nwb/processing/nwb/components/electrode_group/electrode_group_injector.py
@@ -1,12 +1,14 @@
-from rec_to_nwb.processing.tools.validate_parameters import validate_parameters_not_none
+from pynwb import NWBFile
+
+from rec_to_nwb.processing.tools.beartype.beartype import beartype
 
 
 class ElectrodeGroupInjector:
 
-    def inject_all_electrode_groups(self, nwb_content, electrode_groups):
+    @beartype
+    def inject_all_electrode_groups(self, nwb_content: NWBFile, electrode_groups: list):
         """insert electrode groups to nwb file"""
 
-        validate_parameters_not_none(__name__, nwb_content, electrode_groups)
         for electrode_group in electrode_groups:
             self.__inject_electrode_group(nwb_content, electrode_group)
 
