4681e07baffa067dfdb560c8d4e38d854868f9c4
Wojciech Merynda <48327847+WojciechMerynda@users.noreply.github.com>
2020-06-02 12:45:30 +0200

    electrode structure originators (#421)

---
+++ rec_to_nwb/processing/builder/originators/electrode_group_originator.py
@@ -0,0 +1,34 @@
+import os
+import logging.config
+
+from rec_to_nwb.processing.nwb.components.electrode_group.electrode_group_factory import ElectrodeGroupFactory
+from rec_to_nwb.processing.nwb.components.electrode_group.electrode_group_injector import ElectrodeGroupInjector
+from rec_to_nwb.processing.nwb.components.electrode_group.fl_nwb_electrode_group_manager import \
+    FlNwbElectrodeGroupManager
+
+path = os.path.dirname(os.path.abspath(__file__))
+logging.config.fileConfig(fname=str(path) + '/../../../logging.conf', disable_existing_loggers=False)
+logger = logging.getLogger(__name__)
+
+
+class ElectrodeGroupOriginator:
+
+    def __init__(self, metadata):
+        self.fl_nwb_electrode_group_manager = FlNwbElectrodeGroupManager(metadata['electrode groups'])
+        self.electrode_group_creator = ElectrodeGroupFactory()
+        self.electrode_group_injector = ElectrodeGroupInjector()
+
+    def make(self, nwb_content, probes, electrode_groups_valid_map):
+        logger.info('ElectrodeGroups: Building')
+        fl_nwb_electrode_groups = self.fl_nwb_electrode_group_manager.get_fl_nwb_electrode_groups(
+            probes=probes,
+            electrode_groups_valid_map=electrode_groups_valid_map
+        )
+        logger.info('ElectrodeGroups: Creating')
+        nwb_electrode_groups = [
+            self.electrode_group_creator.create_nwb_electrode_group(nwb_electrode_group)
+            for nwb_electrode_group in fl_nwb_electrode_groups
+        ]
+        logger.info('ElectrodeGroups: Injecting into NWB')
+        self.electrode_group_injector.inject_all_electrode_groups(nwb_content, nwb_electrode_groups)
+        return nwb_electrode_groups
---
+++ rec_to_nwb/processing/builder/originators/shanks_electrodes_originator.py
@@ -0,0 +1,30 @@
+import logging.config
+import os
+
+from rec_to_nwb.processing.nwb.components.device.shanks_electrodes.fl_shanks_electrode_manager import \
+    FlShanksElectrodeManager
+from rec_to_nwb.processing.nwb.components.device.shanks_electrodes.shanks_electrode_creator import \
+    ShanksElectrodeCreator
+
+path = os.path.dirname(os.path.abspath(__file__))
+logging.config.fileConfig(fname=str(path) + '/../../../logging.conf', disable_existing_loggers=False)
+logger = logging.getLogger(__name__)
+
+
+class ShanksElectrodeOriginator:
+
+    def __init__(self, probes, metadata):
+        self.fl_shanks_electrode_manager = FlShanksElectrodeManager(probes, metadata['electrode groups'])
+        self.shanks_electrodes_creator = ShanksElectrodeCreator()
+
+    def make(self):
+        logger.info('Probes-ShanksElectrode: Building')
+        fl_shanks_electrodes_dict = self.fl_shanks_electrode_manager.get_fl_shanks_electrodes_dict()
+        logger.info('Probes-ShanksElectrode: Creating')
+        shanks_electrodes_dict = {}
+        for probe_type, fl_shanks_electrodes in fl_shanks_electrodes_dict.items():
+            shanks_electrodes_dict[probe_type] = [
+                self.shanks_electrodes_creator.create(fl_shanks_electrode)
+                for fl_shanks_electrode in fl_shanks_electrodes
+            ]
+        return shanks_electrodes_dict
