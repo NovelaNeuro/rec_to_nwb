9034e25cc8b30f80aa85936c547db43904befa07
acwikla <55695978+acwikla-novela@users.noreply.github.com>
2020-06-12 08:20:19 +0200

    Upload dependencies before test conda package (#449)

---
+++ upload_dependencies.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+export PKG_NAME=rec_to_nwb
+export ANACONDA_API_TOKEN=$CONDA_UPLOAD_TOKEN
+export CONDA_BUILD_PATH=/home/travis/miniconda/envs/test-environment/conda-bld
+
+conda config --set anaconda_upload no
+
+echo "Build missing pypi packages..."
+conda skeleton pypi rec_to_binaries --version 0.5.1.dev0
+conda skeleton pypi xmldiff
+
+echo "Build missing pypi packages into conda packages..."
+conda build rec_to_binaries
+conda build xmldiff
+
+echo "Convert  missing pypi packages ..."
+conda convert --platform osx-64 $CONDA_BUILD_PATH/linux-64/***.tar.bz2 --output-dir $CONDA_BUILD_PATH -q
+conda convert --platform linux-32 $CONDA_BUILD_PATH/linux-64/***.tar.bz2 --output-dir $CONDA_BUILD_PATH -q
+conda convert --platform linux-64 $CONDA_BUILD_PATH/linux-64/***.tar.bz2 --output-dir $CONDA_BUILD_PATH -q
+conda convert --platform win-32 $CONDA_BUILD_PATH/linux-64/***.tar.bz2 --output-dir $CONDA_BUILD_PATH -q
+conda convert --platform win-64 $CONDA_BUILD_PATH/linux-64/***.tar.bz2 --output-dir $CONDA_BUILD_PATH -q
+
+echo "Upload  missing pypi packages to anaconda..."
+anaconda upload $CONDA_BUILD_PATH/**/rec_to_binaries-*.tar.bz2 --force
+anaconda upload $CONDA_BUILD_PATH/**/xmldiff-*.tar.bz2 --force
