b76c2bbe74c09cd4227d065ceaf976c46b141faf
Wojciech Merynda <wmerynda@novelaneuro.com>
2020-06-17 15:28:03 +0200

    update upload

--- conda_upload.sh
+++ conda_upload.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export PKG_NAME=rec_to_nwb
-export ANACONDA_API_TOKEN=$CONDA_UPLOAD_TOKEN
+export ANACONDA_API_TOKEN=$CONDA_UPLOAD_TEST_TOKEN
 export VERSION=$(python setup.py)
 export CONDA_BUILD_PATH=/home/travis/miniconda/envs/test-environment/conda-bld
 export BASE_PATH=$(pwd)
@@ -9,7 +9,7 @@ export BASE_PATH=$(pwd)
 conda config --set anaconda_upload no
 
 echo "Building conda package..."
-conda build . -c novelakrk -c conda-forge --no-include-recipe || exit 1
+conda build . -c acwikla-novela -c novelakrk -c conda-forge --no-include-recipe || exit 1
 
 echo "Move conda package..."
 mv ${CONDA_BUILD_PATH}/linux-64/${PKG_NAME}-${VERSION}-py37_0.tar.bz2  ${CONDA_BUILD_PATH} || exit 1
--- upload_dependencies.sh
+++ upload_dependencies.sh
@@ -13,14 +13,12 @@ conda skeleton pypi vdom
 conda skeleton pypi jp_proxy_widget
 conda skeleton pypi mountainlab_pytools
 
-
 echo "Build missing pypi packages into conda packages..."
 conda build rec_to_binaries
 conda build xmldiff
 conda build vdom
 conda build jp_proxy_widget
 conda build mountainlab_pytools
-
 
 echo "Convert  missing pypi packages ..."
 conda convert --platform osx-64 $CONDA_BUILD_PATH/linux-64/***.tar.bz2 --output-dir $CONDA_BUILD_PATH -q
