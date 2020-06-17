9c9541cdfe0150bc9df8e195c98fcaf44ae53988
Wojciech Merynda <48327847+WojciechMerynda@users.noreply.github.com>
2020-06-17 15:27:23 +0200

    version update (#460)

--- setup.py
+++ setup.py
@@ -13,5 +13,5 @@ setup(
     package_data={'': ['logging.conf', 'data/fl_lab_header.xsd', 'data/reconfig_header.xsd']},
     description='Data transformation from rec binary files into NWB 2.0 format',
     platforms='Posix; MacOS X; Windows',
-    python_requires='==3.7.4',
+    python_requires='>=3.6',
 )
