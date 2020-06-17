65f9a0fd70bf55ca5b372bbce2eac4c171f70fb3
acwikla <acwikla@novelaneuro.com>
2020-06-17 13:17:52 +0200

    Recursive dependencies

--- upload_dependencies.sh
+++ upload_dependencies.sh
@@ -7,15 +7,13 @@ export CONDA_BUILD_PATH=/home/travis/miniconda/envs/test-environment/conda-bld
 conda config --set anaconda_upload no
 
 echo "Build missing pypi packages..."
-conda skeleton pypi rec_to_binaries --version 0.5.1.dev0
-conda skeleton pypi xmldiff
-conda skeleton pypi jp_proxy_widget
-conda skeleton pypi mountainlab_pytools
+conda skeleton pypi rec_to_binaries --version 0.5.1.dev0 --recursive
+conda skeleton pypi xmldiff --recursive
+conda skeleton pypi mountainlab_pytools --recursive
 
 echo "Build missing pypi packages into conda packages..."
 conda build rec_to_binaries
 conda build xmldiff
-conda build jp_proxy_widget
 conda build mountainlab_pytools
 
 echo "Convert  missing pypi packages ..."
@@ -28,5 +26,4 @@ conda convert --platform win-64 $CONDA_BUILD_PATH/linux-64/***.tar.bz2 --output-
 echo "Upload  missing pypi packages to anaconda..."
 anaconda upload $CONDA_BUILD_PATH/**/rec_to_binaries-*.tar.bz2 --force
 anaconda upload $CONDA_BUILD_PATH/**/xmldiff-*.tar.bz2 --force
-anaconda upload $CONDA_BUILD_PATH/**/jp_proxy_widget-*.tar.bz2 --force
 anaconda upload $CONDA_BUILD_PATH/**/mountainlab_pytools-*.tar.bz2 --force
