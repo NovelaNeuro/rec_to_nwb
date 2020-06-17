7407f62c9e8f93650ab210dbe1ccc88fe7cdc953
Wojciech Merynda <wmerynda@novelaneuro.com>
2020-06-17 15:19:08 +0200

    update upload

--- upload_dependencies.sh
+++ upload_dependencies.sh
@@ -7,14 +7,18 @@ export CONDA_BUILD_PATH=/home/travis/miniconda/envs/test-environment/conda-bld
 conda config --set anaconda_upload no
 
 echo "Build missing pypi packages..."
-conda skeleton pypi rec_to_binaries --version 0.5.1.dev0 --recursive
-conda skeleton pypi xmldiff --recursive
-conda skeleton pypi mountainlab_pytools --recursive
+conda skeleton pypi rec_to_binaries --version 0.5.1.dev0
+conda skeleton pypi xmldiff
+conda skeleton pypi mountainlab_pytools
+conda skeleton pypi vdom
+conda skeleton pypi jp_proxy_widget
 
 echo "Build missing pypi packages into conda packages..."
 conda build rec_to_binaries
 conda build xmldiff
 conda build mountainlab_pytools
+conda build vdom
+conda build jp_proxy_widget
 
 echo "Convert  missing pypi packages ..."
 conda convert --platform osx-64 $CONDA_BUILD_PATH/linux-64/***.tar.bz2 --output-dir $CONDA_BUILD_PATH -q
@@ -27,3 +31,5 @@ echo "Upload  missing pypi packages to anaconda..."
 anaconda upload $CONDA_BUILD_PATH/**/rec_to_binaries-*.tar.bz2 --force
 anaconda upload $CONDA_BUILD_PATH/**/xmldiff-*.tar.bz2 --force
 anaconda upload $CONDA_BUILD_PATH/**/mountainlab_pytools-*.tar.bz2 --force
+anaconda upload $CONDA_BUILD_PATH/**/vdom-*.tar.bz2 --force
+anaconda upload $CONDA_BUILD_PATH/**/jp_proxy_widget-*.tar.bz2 --force
