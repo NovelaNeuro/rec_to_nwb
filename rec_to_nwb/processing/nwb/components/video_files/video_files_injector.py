7da92f392cd390387e32e02d9bd18d6b7f61ef9f
Wojciech Merynda <wmerynda@novelaneuro.com>
2020-06-19 14:20:45 +0200

    fl_video_files_extractor.py

--- rec_to_nwb/processing/nwb/components/video_files/video_files_injector.py
+++ rec_to_nwb/processing/nwb/components/video_files/video_files_injector.py
@@ -1,5 +1,12 @@
+from pynwb import NWBFile
+from pynwb.image import ImageSeries
+
+from rec_to_nwb.processing.tools.beartype.beartype import beartype
+
+
 class VideoFilesInjector:
 
     @staticmethod
-    def inject(nwb_content, processing_module_name, image_series):
+    @beartype
+    def inject(nwb_content: NWBFile, processing_module_name: str, image_series: ImageSeries):
         nwb_content.processing[processing_module_name].add(image_series)
