72a936662f4143d0eaf9ed75b0fc9f52979f9657
Wojciech Merynda <wmerynda@novelaneuro.com>
2020-06-19 09:24:18 +0200

    fl_video_files_extractor.py

--- rec_to_nwb/processing/nwb/components/video_files/fl_video_files_extractor.py
+++ rec_to_nwb/processing/nwb/components/video_files/fl_video_files_extractor.py
@@ -1,16 +1,24 @@
-import os
+from rec_to_binaries.read_binaries import readTrodesExtractedDataFile
 
 
 class FlVideoFilesExtractor:
 
-    def __init__(self, datasets, video_directory):
-        self.datasets = datasets
+    def __init__(self, raw_data_path, video_directory, video_files_metadata):
+        self.raw_data_path = raw_data_path
         self.video_directory = video_directory
+        self.video_files_metadata = video_files_metadata
 
     def extract_video_files(self):
-        video_files = os.listdir(self.video_directory)
+        video_files = self.video_files_metadata
         extracted_video_files = []
         for video_file in video_files:
-            new_fl_video_file = {}
-            new_fl_video_file["name"] = str(video_file)
-            new_fl_video_file["timestamps"] = 0
+            new_fl_video_file = {
+                "name": video_file["name"],
+                "timestamps": readTrodesExtractedDataFile(
+                    self.raw_data_path + "/"
+                    + video_file["name"][:-4]
+                    + "videoTimeStamps.cameraHWSync"
+                )["data"],
+                "device": video_file["camera_id"]
+            }
+            extracted_video_files.append(new_fl_video_file)
