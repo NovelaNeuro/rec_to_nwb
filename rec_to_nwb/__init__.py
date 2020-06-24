a2e1c22cc6ee595e2a95d90979cad05f2ef9161b
Wojciech Merynda <48327847+WojciechMerynda@users.noreply.github.com>
2020-06-24 12:12:07 +0200

    direct imports (#468)

--- rec_to_nwb/__init__.py
+++ rec_to_nwb/__init__.py
@@ -1,1 +1,4 @@
 #__init__.py
+from rec_to_nwb.processing.builder.nwb_file_builder import NWBFileBuilder
+from rec_to_nwb.processing.builder.raw_to_nwb_builder import RawToNWBBuilder
+from rec_to_nwb.processing.metadata.metadata_manager import MetadataManager
