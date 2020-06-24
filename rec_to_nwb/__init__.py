9b39be6afc6a9d1faa27d28aaf28a73ed09c6a10
Wojciech Merynda <wmerynda@novelaneuro.com>
2020-06-24 12:01:23 +0200

    direct imports

--- rec_to_nwb/__init__.py
+++ rec_to_nwb/__init__.py
@@ -2,5 +2,3 @@
 from rec_to_nwb.processing.builder.nwb_file_builder import NWBFileBuilder
 from rec_to_nwb.processing.builder.raw_to_nwb_builder import RawToNWBBuilder
 from rec_to_nwb.processing.metadata.metadata_manager import MetadataManager
-
-
