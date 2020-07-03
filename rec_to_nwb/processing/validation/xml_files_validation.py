42ca999623a0582e91d158c2c98424f8466bc4d8
wbodo <55714242+wbodo@users.noreply.github.com>
2020-07-03 15:29:40 +0200

    add checking if xml exist

---
+++ rec_to_nwb/processing/validation/xml_files_validation.py
@@ -0,0 +1,15 @@
+import os
+
+from rec_to_nwb.processing.exceptions.missing_data_exception import MissingDataException
+from rec_to_nwb.processing.validation.validation_registrator import ValidationRegistrator
+from rec_to_nwb.processing.validation.xml_files_validation_summary import XmlFilesValidationSummary
+
+
+class XmlFilesValidator(ValidationRegistrator):
+    def __init__(self, path):
+        self.path = path
+
+    def create_summary(self):
+        if not os.path.exists(self.path):
+            raise MissingDataException('xml file ' + self.path + ' does not exist!')
+        return XmlFilesValidationSummary()
