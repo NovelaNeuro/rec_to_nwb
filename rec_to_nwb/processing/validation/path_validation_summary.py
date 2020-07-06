75149bf1be979fd9380a61b57e6ee9276b838ae0
wbodo <55714242+wbodo@users.noreply.github.com>
2020-07-06 13:03:42 +0200

    add path validator (#479)

---
+++ rec_to_nwb/processing/validation/path_validation_summary.py
@@ -0,0 +1,7 @@
+from rec_to_nwb.processing.validation.validation_summary import ValidationSummary
+
+
+class PathValidationSummary(ValidationSummary):
+
+    def is_valid(self):
+        return True
