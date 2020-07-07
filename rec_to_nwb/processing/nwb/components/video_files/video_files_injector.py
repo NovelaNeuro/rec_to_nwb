faaaa746a48047ef848b1ba354063df39cf7622f
Wojciech Merynda <wmerynda@novelaneuro.com>
2020-07-07 12:39:50 +0200

    video container

--- rec_to_nwb/processing/nwb/components/video_files/video_files_injector.py
+++ rec_to_nwb/processing/nwb/components/video_files/video_files_injector.py
@@ -1,5 +1,5 @@
 from pynwb import NWBFile
-from pynwb.image import ImageSeries
+from pynwb.behavior import BehavioralEvents
 
 from rec_to_nwb.processing.tools.beartype.beartype import beartype
 
@@ -8,5 +8,8 @@ class VideoFilesInjector:
 
     @staticmethod
     @beartype
-    def inject(nwb_content: NWBFile, image_series: ImageSeries):
-        nwb_content.add_stimulus(image_series)
+    def inject(nwb_content: NWBFile, image_series_list: list):
+        video = BehavioralEvents(name='video')
+        for image_series in image_series_list:
+            video.add_timeseries(image_series)
+        nwb_content.processing['behavior'].add(video)
