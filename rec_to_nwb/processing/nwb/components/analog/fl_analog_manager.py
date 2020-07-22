9401bf144cbff6c556b121c3870d66a428d81ff3
wbodo <55714242+wbodo@users.noreply.github.com>
2020-07-22 19:04:33 +0200

    User/wbodo/add row names to analog (#498)

--- rec_to_nwb/processing/nwb/components/analog/fl_analog_manager.py
+++ rec_to_nwb/processing/nwb/components/analog/fl_analog_manager.py
@@ -30,16 +30,25 @@ class FlAnalogManager:
                 )
             )
         merged_epochs = self.__merge_epochs(all_analog_data)
+        description = self.__merge_row_description(all_analog_data)
         analog_data = self.__merge_analog_sensors(merged_epochs)
-        return FlAnalogBuilder.build(analog_data, self.__get_timestamps(merged_epochs))
+        return FlAnalogBuilder.build(analog_data, self.__get_timestamps(merged_epochs), description)
 
     @staticmethod
     def __merge_epochs(data_from_multiple_datasets):
         merged_epochs = data_from_multiple_datasets[0]
         for single_dataset_data in data_from_multiple_datasets[1:]:
-            for analog_file in single_dataset_data.keys():
-                merged_epochs[analog_file] = np.hstack((merged_epochs[analog_file], single_dataset_data[analog_file]))
+            for row in single_dataset_data.keys():
+                merged_epochs[row] = np.hstack((merged_epochs[row], single_dataset_data[row]))
         return merged_epochs
+
+    @staticmethod
+    def __merge_row_description(data_from_multiple_datasets):
+        row_ids = data_from_multiple_datasets[0].keys()
+        description = ''
+        for id in row_ids:
+            description += id + '   '
+        return description
 
     @classmethod
     def __merge_analog_sensors(cls, merged_epochs):
