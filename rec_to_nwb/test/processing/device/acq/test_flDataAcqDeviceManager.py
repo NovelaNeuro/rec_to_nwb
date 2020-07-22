255964a222a43a101c0bafab01f5bcaf15de68b4
Wojciech Merynda <48327847+WojciechMerynda@users.noreply.github.com>
2020-07-22 11:56:46 +0200

    User/wmerynda/acq device changes (#497)

--- rec_to_nwb/test/processing/device/acq/test_flDataAcqDeviceManager.py
+++ rec_to_nwb/test/processing/device/acq/test_flDataAcqDeviceManager.py
@@ -8,8 +8,8 @@ class TestFlDataAcqDeviceManager(TestCase):
 
     def test_fl_data_acq_device_manager_create_successfully_without_optional_data(self):
         metadata = [
-            {'name': 'acq_0', 'system': 'system_0', 'amplifier': 'amplifier_0', 'adc_circuit': 'adc_circuit_0'},
-            {'name': 'acq_1', 'system': 'system_1', 'amplifier': 'amplifier_1', 'adc_circuit': 'adc_circuit_1'}
+            {'name': 'dataacq_device0', 'system': 'system_0', 'amplifier': 'amplifier_0', 'adc_circuit': 'adc_circuit_0'},
+            {'name': 'dataacq_device1', 'system': 'system_1', 'amplifier': 'amplifier_1', 'adc_circuit': 'adc_circuit_1'}
         ]
 
         fl_data_acq_device_manager = FlDataAcqDeviceManager(metadata)
@@ -21,19 +21,19 @@ class TestFlDataAcqDeviceManager(TestCase):
         self.assertIsInstance(fl_data_acq_device[0].amplifier, str)
         self.assertIsInstance(fl_data_acq_device[0].adc_circuit, str)
 
-        self.assertEqual(fl_data_acq_device[0].name, 'acq_0')
+        self.assertEqual(fl_data_acq_device[0].name, 'dataacq_device0')
         self.assertEqual(fl_data_acq_device[0].system, 'system_0')
         self.assertEqual(fl_data_acq_device[0].amplifier, 'amplifier_0')
         self.assertEqual(fl_data_acq_device[0].adc_circuit, 'adc_circuit_0')
-        self.assertEqual(fl_data_acq_device[1].name, 'acq_1')
+        self.assertEqual(fl_data_acq_device[1].name, 'dataacq_device1')
         self.assertEqual(fl_data_acq_device[1].system, 'system_1')
         self.assertEqual(fl_data_acq_device[1].amplifier, 'amplifier_1')
         self.assertEqual(fl_data_acq_device[1].adc_circuit, 'adc_circuit_1')
 
     def test_fl_data_acq_device_manager_create_successfully_with_optional_data(self):
         metadata = [
-            {'name': 'acq_0', 'system': 'system_0', 'amplifier': 'amplifier_0', 'adc_circuit': 'adc_circuit_0'},
-            {'name': 'acq_1', 'system': 'system_1'}
+            {'name': 'dataacq_device0', 'system': 'system_0', 'amplifier': 'amplifier_0', 'adc_circuit': 'adc_circuit_0'},
+            {'name': 'dataacq_device1', 'system': 'system_1'}
         ]
 
         fl_data_acq_device_manager = FlDataAcqDeviceManager(metadata)
@@ -45,11 +45,11 @@ class TestFlDataAcqDeviceManager(TestCase):
         self.assertIsInstance(fl_data_acq_device[0].amplifier, str)
         self.assertIsInstance(fl_data_acq_device[0].adc_circuit, str)
 
-        self.assertEqual(fl_data_acq_device[0].name, 'acq_0')
+        self.assertEqual(fl_data_acq_device[0].name, 'dataacq_device0')
         self.assertEqual(fl_data_acq_device[0].system, 'system_0')
         self.assertEqual(fl_data_acq_device[0].amplifier, 'amplifier_0')
         self.assertEqual(fl_data_acq_device[0].adc_circuit, 'adc_circuit_0')
-        self.assertEqual(fl_data_acq_device[1].name, 'acq_1')
+        self.assertEqual(fl_data_acq_device[1].name, 'dataacq_device1')
         self.assertEqual(fl_data_acq_device[1].system, 'system_1')
         self.assertEqual(fl_data_acq_device[1].amplifier, '')
         self.assertEqual(fl_data_acq_device[1].adc_circuit, '')
