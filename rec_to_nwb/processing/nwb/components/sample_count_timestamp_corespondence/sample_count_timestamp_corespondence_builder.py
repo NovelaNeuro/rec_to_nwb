08be89d157118e6282a512759418514515ea122c
wbodo <55714242+wbodo@users.noreply.github.com>
2020-08-11 10:25:01 +0200

    add manager test

--- rec_to_nwb/processing/nwb/components/sample_count_timestamp_corespondence/sample_count_timestamp_corespondence_builder.py
+++ rec_to_nwb/processing/nwb/components/sample_count_timestamp_corespondence/sample_count_timestamp_corespondence_builder.py
@@ -8,7 +8,7 @@ class SampleCountTimestampCorespondenceBuilder:
     def build(self):
         return TimeSeries(name="SampleCountTimestampCorespondence",
                           description="Corespondence between sample count and timestamps",
-                          data=self.data[0],
-                          timestamps=self.data[1],
+                          data=self.data[:, 0],
+                          timestamps=self.data[:, 1],
                           unit='int64'
                           )
--- rec_to_nwb/processing/nwb/components/sample_count_timestamp_corespondence/sample_count_timestamp_corespondence_manager.py
+++ rec_to_nwb/processing/nwb/components/sample_count_timestamp_corespondence/sample_count_timestamp_corespondence_manager.py
@@ -7,9 +7,9 @@ from rec_to_nwb.processing.nwb.components.sample_count_timestamp_corespondence.s
 class SampleCountTimestampCorespondenceManager:
     def __init__(self, datasets):
         self.continuous_time_files = [dataset.get_continuous_time() for dataset in datasets]
+        self.extractor = SampleCountTimestampCorespondenceExtractor(self.continuous_time_files)
 
     def get_timeseries(self):
-        extractor = SampleCountTimestampCorespondenceExtractor(self.continuous_time_files)
-        data = extractor.extract()
+        data = self.extractor.extract()
         builder = SampleCountTimestampCorespondenceBuilder(data)
         return builder.build()
