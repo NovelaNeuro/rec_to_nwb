06d252da6860ccd79d3c09096a77a1c4b78b5b1d
wbodo <55714242+wbodo@users.noreply.github.com>
2020-08-11 07:59:19 +0200

    User/wbodo/corespondence between timestamps and sample count (#514)
    * add extractor and builder
    
    * add manager

---
+++ rec_to_nwb/processing/nwb/components/sample_count_timestamp_corespondence/sample_count_timestamp_corespondence_extractor.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+from rec_to_binaries.read_binaries import readTrodesExtractedDataFile
+
+
+class SampleCountTimestampCorespondenceExtractor:
+    def __init__(self, files):
+        self.files = files
+
+    def extract(self):
+        data = []
+        for file in self.files:
+            data.append(self.__get_continuous_time_data_from_single_file(file))
+        merged_data = self.__merge_data_from_multiple_files(data)
+
+    def __merge_data_from_multiple_files(self, data):
+        merged_data = np.vstack(data)
+        return merged_data
+
+    def __get_continuous_time_data_from_single_file(self, continuous_time_file):
+        continuous_time = readTrodesExtractedDataFile(continuous_time_file)
+        new_array = np.ndarray(shape=(len(continuous_time['data']), 2), dtype='int64')
+        for i, single_timestamp in enumerate(continuous_time['data']):
+            new_array[i, 0] = single_timestamp[0]
+            new_array[i, 1] = single_timestamp[3]
+        return new_array
