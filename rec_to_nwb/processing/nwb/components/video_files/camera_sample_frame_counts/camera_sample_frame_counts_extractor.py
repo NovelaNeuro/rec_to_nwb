da85ac66143e5d8f2191897eec4566a401f1faa2
wbodo <55714242+wbodo@users.noreply.github.com>
2020-08-11 14:26:33 +0200

    new extractor

--- rec_to_nwb/processing/nwb/components/video_files/camera_sample_frame_counts/camera_sample_frame_counts_extractor.py
+++ rec_to_nwb/processing/nwb/components/video_files/camera_sample_frame_counts/camera_sample_frame_counts_extractor.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from rec_to_binaries.read_binaries import readTrodesExtractedDataFile
 
 
@@ -7,13 +9,17 @@ class CameraSampleFrameCountsExtractor:
         self.raw_data_path = raw_data_path
 
     def extract_camera_hw_frame_count(self, hw_frame_count_filename):
-        timestamps = readTrodesExtractedDataFile(
-                    self.pos_data_path + "/" + hw_frame_count_filename + "pos_cameraHWFrameCount.dat"
-                )["data"]
-        return timestamps
+        content = readTrodesExtractedDataFile(
+                  self.pos_data_path + "/" + hw_frame_count_filename + "pos_cameraHWSync"
+                  )["data"]
+        camera_sample_frame_counts = np.ndarray(shape = (len(content), 2), dtype='uint32')
+        for i, record in enumerate(content):
+            camera_sample_frame_counts[i, 0] = record[0]
+            camera_sample_frame_counts[i, 1] = record[1]
+        return camera_sample_frame_counts
 
-    def extract_timestamps(self, hw_sync_filename):
-        timestamps = readTrodesExtractedDataFile(
-                    self.raw_data_path + "/" + hw_sync_filename + "videoTimeStamps"
-                )["data"]
-        return timestamps
+    # def extract_timestamps(self, video_timestamps_filename):
+    #     # timestamps = readTrodesExtractedDataFile(
+    #     #             self.raw_data_path + "/" + video_timestamps_filename + "videoTimeStamps"
+    #     #         )["data"]
+    #     return timestamps
