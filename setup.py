2265958927107b80e5f19bdeb1985839ce331cae
Wojciech Merynda <wmerynda@novelaneuro.com>
2020-09-01 12:22:07 +0200

    new schema

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     author='Novela Neurotech',
     url="https://github.com/NovelaNeuro/rec_to_nwb",
     packages=find_packages(),
-    package_data={'': ['logging.conf', 'data/fl_lab_header.xsd', 'data/reconfig_header.xsd', 'data/default_header.xml']},
+    package_data={'': ['logging.conf', 'data/fl_lab_header.xsd', 'data/header_schema.xsd', 'data/default_header.xml']},
     description='Data transformation from rec binary files into NWB 2.0 format',
     platforms='Posix; MacOS X; Windows',
     python_requires='>=3.6'
