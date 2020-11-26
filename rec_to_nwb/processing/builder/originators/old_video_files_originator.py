ae8929188038aac57e0cfee037a5e90aad7cb181
Ji Hyun Bak <jihyun.bak@gmail.com>
2020-11-25 16:03:18 -0800

    fix name typo in OldVideoFilesOriginator

--- rec_to_nwb/processing/builder/originators/old_video_files_originator.py
+++ rec_to_nwb/processing/builder/originators/old_video_files_originator.py
@@ -11,10 +11,9 @@ class OldVideoFilesOriginator:
         self.old_fl_video_files_manager = OldFlVideoFilesManager(raw_data_path, video_path, video_files_metadata)
 
     def make(self, nwb_content):
-        fl_video_files = self.fl_video_files_manager.get_video_files()
+        fl_video_files = self.old_fl_video_files_manager.get_video_files()
         image_series_list = [
             VideoFilesCreator.create(fl_video_file, self.video_directory, nwb_content)
             for fl_video_file in fl_video_files
         ]
         VideoFilesInjector.inject_all(nwb_content, image_series_list)
-
