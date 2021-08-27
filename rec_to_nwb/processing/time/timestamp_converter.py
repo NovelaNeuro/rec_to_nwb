7a426ad2a8dd803956949de008425aed27fec751
Eric Denovellis <edenovellis@gmail.com>
2021-08-27 11:05:21 -0700

    Add more documentation

--- rec_to_nwb/processing/time/timestamp_converter.py
+++ rec_to_nwb/processing/time/timestamp_converter.py
@@ -17,34 +17,45 @@ class TimestampConverter:
 
     @staticmethod
     def convert_timestamps(continuous_times, timestamps):
+        '''Matches the trodes timestamp index from the camera to the adjusted
+        timestamps (in unix time) from the ephys recording.
+
+        The adjusted timestamps are the ephys recoding timestamps adjusted for
+        jitter from the arrival times of packets from the MCU.
+
+        Timestamps from the camera that do not having matching timestamps from
+        the ephys recording will be marked as NaN. This can happen when the
+        position tracking is shut off after the ephys recording is done or
+        started before the ephys recording starts.
+
+        Parameters
+        ----------
+        continuous_times: ndarray, shape (2, n_ephys_time)
+            From the continuous time file
+            row 0: trodestime, row 1: adjusted_systime_
+        timestamps: ndarray, shape (n_position_time, )
+            trodes timestamps relative to camera’s timing (from pos_online.dat)
+
+        Returns
+        -------
+        converted_timestamps : ndarray, shape (n_position_time,)
+            Timestamps from the position tracking in terms of the adjusted
+
         '''
-        continuous_times: (2, T) numpy array, where T is the number of timepoints
-                        row 0: trodestime, row 1: adjusted_systime
-        timestamps: trodes timestamps relative to camera’s timing (from pos)
-        '''
-        # add values at the end of continuous_times to make sure all values are within the range
+        # add values at the end of continuous_times to make sure all values are
+        # within the range
         max_vals = np.asarray(
-            [[np.iinfo(np.int64).max], [np.iinfo(np.int64).max]], dtype=np.int64)
+            [[np.iinfo(np.int64).max],
+             [np.iinfo(np.int64).max]],
+            dtype=np.int64)
         continuous_times = np.hstack((continuous_times, max_vals))
 
-        # look up the timestamps in the first row of continuous_times
+        # Find the matching timestamp index (trodestime)
         timestamp_ind = np.searchsorted(continuous_times[0, :], timestamps)
         converted_timestamps = (continuous_times[1, timestamp_ind] /
                                 NANOSECONDS_PER_SECOND)
 
         # get rid of any that are not exact
         not_found = timestamps != continuous_times[0, timestamp_ind]
-        #print(f'in Timestamp Converter: {sum(not_found)} timestamps not found in continuous time file')
         converted_timestamps[not_found] = np.nan
         return converted_timestamps
-
-        # old code
-        # converted_timestamps = np.ndarray(shape=[len(timestamps), ], dtype="float64")
-        # for i, timestamp in enumerate(timestamps):
-        #     key = str(timestamp)
-        #     value = continuous_time_dict.get(key, float('nan')) / 1E9
-        #     if np.isnan(value):
-        #         message = 'Following key: ' + str(key) + ' does not exist in continioustime dictionary!'
-        #         logger.exception(message)
-        #     converted_timestamps[i] = value
-        # return converted_timestamps
