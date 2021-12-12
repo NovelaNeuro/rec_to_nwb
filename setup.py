d9636217bdbb68ccb9bdef3fdd937e94d2ac8c6b
Eric Denovellis <edeno@bu.edu>
2021-12-12 15:06:55 -0500

    Fix missing comma

--- setup.py
+++ setup.py
@@ -1,8 +1,7 @@
 version = '0.1.020'
 print(version)
 
-from setuptools import setup, find_packages
-
+from setuptools import find_packages, setup
 
 setup(
     name='rec_to_nwb',
@@ -10,10 +9,11 @@ setup(
     author='Novela Neurotech',
     url="https://github.com/NovelaNeuro/rec_to_nwb",
     packages=find_packages(),
-    package_data={'': ['logging.conf', 'data/fl_lab_header.xsd', 'data/header_schema.xsd', 'data/default_header.xml']},
+    package_data={'': ['logging.conf', 'data/fl_lab_header.xsd',
+                       'data/header_schema.xsd', 'data/default_header.xml']},
     description='Data transformation from rec binary files into NWB 2.0 format',
     platforms='Posix; MacOS X; Windows',
-    python_requires='>=3.6'
+    python_requires='>=3.6',
     install_requires=[
         'rec_to_binaries>=0.6.12'
     ]
