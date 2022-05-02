b38dae2b3087c73fd6ab9bc24ac1715c1dd011dd
Loren <loren@phy.ucsf.edu>
2022-05-02 14:07:59 -0700

    added print for debugging

--- rec_to_nwb/processing/validation/associated_files_validator.py
+++ rec_to_nwb/processing/validation/associated_files_validator.py
@@ -8,6 +8,7 @@ class AssociatedFilesExistanceValidator:
 
     def files_exist(self):
         for associated_file in self.associated_files:
+            print(f'Checking associated file {associated_file["path"]}')
             if not os.path.isfile(associated_file["path"]):
                 return False
         return True
