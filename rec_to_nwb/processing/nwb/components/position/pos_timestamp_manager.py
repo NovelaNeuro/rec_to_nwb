7d0790c9dcab1c4ff3719dd28652db1c92955e07
Eric Denovellis <edeno@bu.edu>
2022-05-25 12:47:40 -0400

    Unique returns a numpy array instead of a pandas series.

--- rec_to_nwb/processing/nwb/components/position/pos_timestamp_manager.py
+++ rec_to_nwb/processing/nwb/components/position/pos_timestamp_manager.py
@@ -32,7 +32,7 @@ class PosTimestampManager(TimestampManager):
         pos_online = readTrodesExtractedDataFile(
             self.directories[dataset_id][0])
         position = pd.DataFrame(pos_online['data'])
-        return position.time.unique().to_numpy(dtype='int64')
+        return position.time.unique().astype(int)
 
     def retrieve_real_timestamps(self, dataset_id):
         """Gets the corresponding Trodes timestamps from the online position
