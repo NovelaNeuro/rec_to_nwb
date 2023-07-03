626c9d6fb958402c579693936acee3ea872aa9d3
Eric Denovellis <edenovellis@gmail.com>
2023-07-03 11:13:40 -0700

    If no DOB, continue

--- rec_to_nwb/processing/metadata/metadata_extractor.py
+++ rec_to_nwb/processing/metadata/metadata_extractor.py
@@ -9,10 +9,12 @@ class MetadataExtractor:
         with open(metadata_path, "r") as stream:
             yaml_dict = yaml.safe_load(stream)
 
-            # yaml automatically converts date_of_birth to a datetime object, need to convert back
-            yaml_dict["subject"]["date_of_birth"] = yaml_dict["subject"][
-                "date_of_birth"
-            ].strftime("%Y-%m-%dT%H:%M:%S.%fZ")
-
+            try:
+                # yaml automatically converts date_of_birth to a datetime object, need to convert back
+                yaml_dict["subject"]["date_of_birth"] = yaml_dict["subject"][
+                    "date_of_birth"
+                ].strftime("%Y-%m-%dT%H:%M:%S.%fZ")
+            except KeyError:
+                pass
             # for some reason they want to convert all ints, float to strings
             return json.loads(json.dumps(yaml_dict), parse_int=str, parse_float=str)
